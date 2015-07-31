ccd4fd387511702ee68f271c7e65db9b15a22ad8
Marco Dorantes <marcodorantes@hotmail.com>
2015-07-31 04:17:20 -0500

    What's New in C# 6

---
+++ Solution1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace ConsoleApplication1
+{
+    class X
+    {
+        public X()
+        {
+            Name = "uno";
+        }
+        public string Name { get; }
+        public int Age => Name.Length;
+        public override string ToString() => $"({Name},{Age})";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new X();
+            WriteLine("X: {0} {1} | {2}",x.Name, x.Age, x);
+        }
+    }
+}
