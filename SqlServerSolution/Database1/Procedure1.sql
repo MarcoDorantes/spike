d1a5a50d0c8c48bbe007d6a22bcf9a54cb11b17b
Marco Dorantes <marcodorantes@hotmail.com>
2015-09-03 01:41:22 -0500

    Links

--- SqlServerSolution/Database1/Procedure1.sql
+++ SqlServerSolution/Database1/Procedure1.sql
@@ -1,4 +1,7 @@
-﻿CREATE PROCEDURE [dbo].[Procedure1]
+﻿--https://msdn.microsoft.com/en-us/library/jj851226(v=vs.103).aspx
+--https://msdn.microsoft.com/en-us/library/jj851212(v=vs.103).aspx
+--http://blogs.msdn.com/b/ssdt/archive/2012/12/07/getting-started-with-sql-server-database-unit-testing-in-ssdt.aspx
+CREATE PROCEDURE [dbo].[Procedure1]
 	@param1 int = 0,
 	@param2 int
 AS
