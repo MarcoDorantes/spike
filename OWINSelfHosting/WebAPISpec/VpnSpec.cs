bfad46aa9322cb416e499a0a876cf9f1f7de88e1
marcodorantes <marcodorantes@hotmail.com>
2015-10-09 14:00:03 -0500

    .NET API wrapper as RESTful service initial draft.

---
+++ OWINSelfHosting/WebAPISpec/VpnSpec.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Http;
+
+/*
+*** Reliable ***
+
+POST REQUEST:
+	URL	: api/reliable
+	Payload	: 
+		<SendRequest>
+			<vpn>registeredVPN</vpn>
+			<destination.type>topic</destination.type>
+			<destination>topic</destination>
+			<correlationID>integer</correlationID>
+			<message>text-message</message>
+		</SendRequest>
+
+POST RESPONSE:
+		<SendResponse>
+			<correlationID>integer</correlationID>
+			<status>enum</status>
+		</SendResponse>
+
+PUT REQUEST:
+	URL	: api/queue
+	Payload	: 
+		<SendRequest>
+			<vpn>registeredVPN</vpn>
+			<destination>queue</destination>
+			<correlationID>integer</correlationID>
+			<message>text-message</message>
+		</SendRequest>
+
+Ejemplos:
+A tópico:
+	URL	: POST VPN_1/topic
+	Payload	: 
+		<destination>topicpathj</destination>
+		<message>8=FIX ...</message>
+
+
+A queue:
+	URL	: PUT VPN_2/queue
+	Payload	: 
+		<destination>QUEUE1</destination>
+		<message>8=FIX ...</message>
+
+*** Guaranteed ***
+
+  POST REQUEST:
+	URL	: api/guaranteed
+
+  */
+
+namespace WebAPISpec
+{
+    public class VpnController : ApiController
+    {
+        // POST api/value
+        public SendResponse Post([FromBody]SendRequest request) { return null; }
+
+
+    }
+
+    [TestClass]
+    public class VpnSpec
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
