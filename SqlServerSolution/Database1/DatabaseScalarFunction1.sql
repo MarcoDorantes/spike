d54eaace10b33c579737db02d858a4cf4c09d526
Marco Dorantes <marcodorantes@hotmail.com>
2015-10-10 13:55:37 -0500

    Added CachedConfiguredSchemaAndHostNameOrIPAndPort map.

---
+++ SqlServerSolution/Database1/DatabaseScalarFunction1.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[DatabaseScalarFunction1]
+(
+	@param1 int,
+	@param2 int
+)
+RETURNS INT
+AS
+BEGIN
+	RETURN @param1 + @param2
+END
+/*
+USE db_;
+GO
+IF OBJECT_ID (N'dbo.GetSolSenderClientConfiguredSchemaAndHostNameOrIPAndPort', N'FN') IS NOT NULL
+    DROP FUNCTION dbo.GetSolSenderClientConfiguredSchemaAndHostNameOrIPAndPort
+GO
+CREATE FUNCTION dbo.GetSolSenderClientConfiguredSchemaAndHostNameOrIPAndPort (@vpn VARCHAR(250))
+RETURNS VARCHAR(250)
+WITH EXECUTE AS CALLER
+AS
+BEGIN
+     RETURN('https://hostname:7000/');
+END;
+SELECT dbo.ISOweek(CONVERT(DATETIME,'12/26/2004',101)) AS 'ISO Week';
+*/
--- SqlServerSolution/Database1/SqlStoredProcedure1.cs
+++ SqlServerSolution/Database1/SqlStoredProcedure1.cs
@@ -3,21 +3,62 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
+using System.Collections.Concurrent;
 
 /*
 Getting Started with CLR Integration
 https://msdn.microsoft.com/en-us/library/ms131052(v=sql.110).aspx
 */
-public partial class StoredProcedures
+//public partial class StoredProcedures
+public static class SolSenderClientStoredProcedures
 {
-  [Microsoft.SqlServer.Server.SqlProcedure]
-  public static void WriteMessageToQueue(string vpn, string queuename, string message, int correlationID)
-  {
-    //SqlContext.Pipe.ExecuteAndSend(cmd);
+  private static readonly ConcurrentDictionary<string,string> CachedConfiguredSchemaAndHostNameOrIPAndPort;
 
-    string baseAddress = GetConfiguredSchemaAndHostNameOrIPAndPort(vpn);
+  static SolSenderClientStoredProcedures()
+  {
+    CachedConfiguredSchemaAndHostNameOrIPAndPort = new ConcurrentDictionary<string, string>();
+  }
+
+  [Microsoft.SqlServer.Server.SqlProcedure]
+  public static void Reset()
+  {
+    if (CachedConfiguredSchemaAndHostNameOrIPAndPort == null)
+    {
+      throw new InvalidOperationException("Internal cache was not properly created.");
+    }
+    CachedConfiguredSchemaAndHostNameOrIPAndPort.Clear();
+  }
+
+  [Microsoft.SqlServer.Server.SqlProcedure]
+  public static string PublishMessage(string vpn, string topicpath, string message, int correlationID)
+  {
+    #region Parameter validation
+    if (string.IsNullOrWhiteSpace(vpn))
+    {
+      throw new ArgumentNullException("vpn", "VPN cannot be empty or null.");
+    }
+    else
+    {
+      vpn = vpn.Trim();
+    }
+
+    if (string.IsNullOrWhiteSpace(topicpath))
+    {
+      throw new ArgumentNullException("topicpath", "topicpath cannot be empty or null.");
+    }
+    else
+    {
+      topicpath = topicpath.Trim();
+    }
+    #endregion
+
+    //SqlContext.Pipe.ExecuteAndSend(cmd);?
+
+    string result = "";
+    string base_address = GetConfiguredSchemaAndHostNameOrIPAndPort(vpn);
+
     const string posted_datakey = "posted2";
-    var request = System.Net.WebRequest.Create(baseAddress + "api/values") as System.Net.HttpWebRequest;
+    var request = System.Net.WebRequest.Create(base_address + "api/values") as System.Net.HttpWebRequest;
     request.Method = "POST";
     request.ContentType = "application/xml; charset=utf-8";
     var payload = System.Text.Encoding.UTF8.GetBytes("<string xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/\">" + posted_datakey + "</string>");
@@ -34,26 +75,81 @@ public partial class StoredProcedures
         string response_payload = reader.ReadToEnd();
         if (response_payload.Length <= 0)
         {
-          throw new Exception("no response");
+          throw new Exception("no response" + "<correlationid> + <status> + <description>"); //exception flow
         }
+        result = "<correlationid> + <status> + <description>"; //normal flow
       }
+      return result;
     }
   }
 
   private static string GetConfiguredSchemaAndHostNameOrIPAndPort(string vpn)
   {
-    //TODO static cache | each hit | cancelable/unloadable static cache
-
-    string result = "";//https://hostname:7000/
-
-    //result = f(vpn); //https://msdn.microsoft.com/es-es/library/ms186755(v=sql.110).aspx
-
+    string result = GetCachedConfiguredSchemaAndHostNameOrIPAndPort(vpn);
+    if (string.IsNullOrWhiteSpace(result))
+    {
+      string url = QueryForConfiguredSchemaAndHostNameOrIPAndPort(vpn);
+      SetCachedConfiguredSchemaAndHostNameOrIPAndPort(vpn, url);
+      result = url;
+    }
     return result;
   }
 
-  /*private static string GetBaseAddress(string hostname_or_IP_and_port)
+  private static string GetCachedConfiguredSchemaAndHostNameOrIPAndPort(string vpn)
   {
-    const string baseAddress_template = "http://{0}/";
-    return string.Format(baseAddress_template, hostname_or_IP_and_port);
-  }*/
+    //static reset-able cache | -- each hit | cancelable/unloadable static cache
+    string result = null;
+    if (CachedConfiguredSchemaAndHostNameOrIPAndPort != null)
+    {
+      if (!CachedConfiguredSchemaAndHostNameOrIPAndPort.TryGetValue(vpn, out result))
+      {
+        result = null;
+      }
+    }
+    else
+    {
+      throw new InvalidOperationException("Internal cache was not properly created.");
+    }
+    return result;
+  }
+  private static void SetCachedConfiguredSchemaAndHostNameOrIPAndPort(string vpn, string url)
+  {
+    if (CachedConfiguredSchemaAndHostNameOrIPAndPort == null)
+    {
+      throw new InvalidOperationException("Internal cache was not properly created.");
+    }
+    if (!CachedConfiguredSchemaAndHostNameOrIPAndPort.TryAdd(vpn, url))
+    {
+      throw new Exception("Target URL cannot be added to internal cache.");
+    }
+  }
+
+  /// <summary>
+  /// Get URL to send requests.  In the exact format: https://hostname:7000/
+  /// https://msdn.microsoft.com/es-es/library/ms186755(v=sql.110).aspx
+  /// https://msdn.microsoft.com/es-es/library/ms131043(v=sql.110).aspx
+  /// </summary>
+  /// <param name="vpn">Key into the index of registered VPNs at SolSenderService-side.</param>
+  /// <returns>Target URL for requests.</returns>
+  private static string QueryForConfiguredSchemaAndHostNameOrIPAndPort(string vpn)
+  {
+    if (string.IsNullOrWhiteSpace(vpn))
+    {
+      throw new ArgumentNullException("vpn", "QueryForConfiguredSchemaAndHostNameOrIPAndPort was called with an empty or null argument value.");
+    }
+
+    string result = "";
+    using (var connection = new SqlConnection("context connection=true"))
+    {
+      connection.Open();
+      var command = new SqlCommand(string.Format("SELECT dbo.DatabaseScalarFunction1('{0}') AS RequestAddress", vpn), connection);
+      object value = command.ExecuteScalar();
+      if (value == null)
+      {
+        throw new Exception("dbo.DatabaseScalarFunction1 returned an empty or null value.");
+      }
+      result = value.ToString();
+    }
+    return result;
+  }
 }
