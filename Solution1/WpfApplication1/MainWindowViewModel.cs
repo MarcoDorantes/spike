5fd8a3fe3d5a05330c0ef263fb3dc70b5bebadbb
Marco Dorantes <marcodorantes@hotmail.com>
2015-10-30 04:11:30 -0600

    mview2

---
+++ Solution1/WpfApplication1/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+  class MainWindowViewModel : INotifyPropertyChanged
+  {
+    private uint tabcount;
+    public MainWindowViewModel()
+    {
+      tabcount = 0;
+      Viewers = new ObservableCollection<Viewer>();
+      AddViewer();
+    }
+
+    public ObservableCollection<Viewer> Viewers { get; private set; }
+    private void AddViewer()
+    {
+      ++tabcount;
+      Viewers.Add(new Viewer { Name = $"Tab{tabcount}", Action = $"Action{tabcount}", Data = new UserControl1ViewModel($"{tabcount}") });
+    }
+
+
+    private ICommand onQuit;
+    public ICommand OnQuit { get { return onQuit ?? (onQuit = new Command(() => Quit())); } }
+    private void Quit() { System.Windows.Application.Current.MainWindow.Close(); }
+
+    private ICommand onNew;
+    public ICommand OnNew { get { return onNew ?? (onNew = new Command(() => New())); } }
+    private void New() { AddViewer(); }
+
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void Notify(string property)
+    {
+      PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(property));
+    }
+    #endregion
+  }
+
+  class Viewer
+  {
+    public string Name { get; set; }
+    public string Action { get; set; }
+    public UserControl1ViewModel Data { get; set; }
+
+    private ICommand onRun;
+    public ICommand OnRun { get { return onRun ?? (onRun = new Command(() => Run())); } }
+    private void Run() { System.Windows.MessageBox.Show($"Run: {Name} - {Action} ({(Data as UserControl1ViewModel).ID})"); }
+  }
+
+  class Command : ICommand
+  {
+    private Action action;
+    public Command(Action call)
+    {
+      action = call;
+    }
+    public event EventHandler CanExecuteChanged;
+    public bool CanExecute(object parameter){return true;}
+    public void Execute(object parameter){ action(); }
+  }
+}
---
+++ Solution1/WpfApplication1/UserControl1ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+  class UserControl1ViewModel : INotifyPropertyChanged
+  {
+    public UserControl1ViewModel(string id)
+    {
+      ID = id;
+      System.Diagnostics.Trace.WriteLine($"[{DateTime.Now.ToString("s")}] UserControl1ViewModel {ID}");
+      Names = new ObservableCollection<string>();
+      Enumerable.Range(0, 5).ToList().ForEach(n => Names.Add($"Item{n}"));
+    }
+    public string ID { get; private set; }
+    public ObservableCollection<string> Names { get; private set; }
+
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void Notify(string property)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+    }
+    #endregion
+  }
+}
