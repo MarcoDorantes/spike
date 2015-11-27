5ed513dfaaa2bc5eaf2f7ed0d783ff9345538c24
marcodorantes <marcodorantes@hotmail.com>
2015-11-27 13:09:16 -0600

    Added GetParts()

--- Telemetry/TelemetrySpec/TelemetryFormatterV2Spec.cs
+++ Telemetry/TelemetrySpec/TelemetryFormatterV2Spec.cs
@@ -28,7 +28,7 @@ namespace TelemetrySpec
     public TelemetryFormatterV2()
     {
       formatters = new Dictionary<string, ITelemetryFormatter>();
-      formatters.Add("S",new Formatter1());
+      formatters.Add("S", new Formatter1());
     }
     public string GetStateNotice(string message_type)
     {
@@ -47,6 +47,40 @@ namespace TelemetrySpec
       return headers.Aggregate(new StringBuilder(), (whole, next) => whole.AppendFormat("{0}", next)).ToString();
     }
   }
+
+  #region configs
+  public class WnConfigurationSection : System.Configuration.ConfigurationSection
+  {
+    [System.Configuration.ConfigurationProperty("Parts", IsRequired = true)]
+    [System.Configuration.ConfigurationCollection(typeof(PartCollection), AddItemName = "Part")]
+    public PartCollection Parts { get { return (PartCollection)this["Parts"]; } }
+  }
+
+  public class PartCollection : System.Configuration.ConfigurationElementCollection
+  {
+    protected override System.Configuration.ConfigurationElement CreateNewElement()
+    {
+      return new PartCollectionElement();
+    }
+    protected override object GetElementKey(System.Configuration.ConfigurationElement element)
+    {
+      return ((PartCollectionElement)element).Name;
+    }
+  }
+
+  public class PartCollectionElement : System.Configuration.ConfigurationElement
+  {
+    public const string DefaultProviderName = "Provider0";
+    public const string DefaultProviderType = "ProviderAssembly.Provider0, ProviderAssembly";
+
+    [System.Configuration.ConfigurationProperty("Name", IsRequired = true, DefaultValue = DefaultProviderName)]
+    public string Name { get { return (string)this["Name"]; } set { this["Name"] = value; } }
+
+    [System.Configuration.ConfigurationProperty("Provider", IsRequired = true, DefaultValue = DefaultProviderType)]
+    public string Provider { get { return (string)this["Provider"]; } }
+  }
+
+  #endregion
 
   [TestClass]
   public class TelemetryFormatterV2Spec
@@ -76,6 +110,82 @@ namespace TelemetrySpec
 
       //Assert
       Assert.AreEqual<string>(expected_payload, payload);
+    }
+
+    [TestMethod]
+    public void ConfigDefaults()
+    {
+      //Arrange
+      WnConfigurationSection existing_section;
+
+      //Act
+      existing_section = System.Configuration.ConfigurationManager.GetSection("WnConfigurationSection") as WnConfigurationSection;
+      var parts = existing_section.Parts.OfType<PartCollectionElement>();
+      var non_existing_section = new WnConfigurationSection();
+      var non_existing_part = new PartCollectionElement();
+
+      //Assert
+      Assert.IsNotNull(existing_section);
+      Assert.IsNotNull(existing_section.Parts);
+      Assert.AreEqual<int>(2, existing_section.Parts.Count);
+      Assert.AreEqual<int>(2, parts.Count());
+      Assert.AreEqual<string>("name1", parts.ElementAt(0).Name);
+      Assert.AreEqual<string>("ProviderAssembly.Provider1, ProviderAssembly", parts.ElementAt(0).Provider);
+      Assert.AreEqual<string>("name2", parts.ElementAt(1).Name);
+      Assert.AreEqual<string>("Provider2, Provider2Assembly", parts.ElementAt(1).Provider);
+
+      Assert.IsNotNull(non_existing_section);
+      Assert.IsFalse(non_existing_section.ElementInformation.IsCollection);
+      Assert.AreEqual<Type>(typeof(WnConfigurationSection), non_existing_section.ElementInformation.Type);
+
+      Assert.IsNotNull(non_existing_part);
+      Assert.AreEqual<string>(PartCollectionElement.DefaultProviderName, non_existing_part.Name);
+      Assert.AreEqual<string>(PartCollectionElement.DefaultProviderType, non_existing_part.Provider);
+    }
+
+    IEnumerable<PartCollectionElement> GetParts()
+    {
+      WnConfigurationSection existing_section = System.Configuration.ConfigurationManager.GetSection("WnConfigurationSection") as WnConfigurationSection;
+      if (existing_section != null)
+      {
+        foreach (var part in existing_section.Parts.OfType<PartCollectionElement>())
+        {
+          yield return part;
+        }
+      }
+      else
+      {
+        yield return new PartCollectionElement();
+      }
+    }
+
+    [TestMethod]
+    public void GettingParts()
+    {
+      //Arrange
+      IEnumerable<PartCollectionElement> parts;
+
+      //Act
+      parts = GetParts();
+
+      //Assert
+      Assert.IsNotNull(parts);
+      switch (parts.Count())
+      {
+        case 1:
+          Assert.AreEqual<string>(PartCollectionElement.DefaultProviderName, parts.ElementAt(0).Name);
+          Assert.AreEqual<string>(PartCollectionElement.DefaultProviderType, parts.ElementAt(0).Provider);
+          break;
+        case 2:
+          Assert.AreEqual<string>("name1", parts.ElementAt(0).Name);
+          Assert.AreEqual<string>("ProviderAssembly.Provider1, ProviderAssembly", parts.ElementAt(0).Provider);
+          Assert.AreEqual<string>("name2", parts.ElementAt(1).Name);
+          Assert.AreEqual<string>("Provider2, Provider2Assembly", parts.ElementAt(1).Provider);
+          break;
+        default:
+          Assert.Fail("parts.Count={0}", parts.Count());
+          break;
+      }
     }
   }
 }
