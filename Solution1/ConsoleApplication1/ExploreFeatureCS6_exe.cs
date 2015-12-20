b17e1e81e9ccb41f064dff40f13ae7b4695ff73d
Marco Dorantes <marcodorantes@hotmail.com>
2015-12-19 18:43:38 -0600

    Make room for more exes.

---
+++ Solution1/ConsoleApplication1/ExploreFeatureCS6_exe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApplication1
+{
+  class X
+  {
+    public X()
+    {
+      Name = "uno";
+    }
+    public string Name { get; }
+    public int Age => Name.Length;
+    public override string ToString() => $"({Name},{Age})";
+  }
+  class ExploreFeatureCS6_exe
+  {
+    static bool log(Exception ex) { WriteLine($"An exception ocurred: {ex.GetType().FullName}"); return false; }
+    static void with_logged_exception(int n)
+    {
+      try
+      {
+        int r = 10 / n;
+      }
+      catch (Exception ex) when (log(ex))
+      {
+        WriteLine("ex is null");
+      }
+    }
+    static void exception_filter()
+    {
+      with_logged_exception(0);
+      WriteLine("Done.");
+    }
+
+    public static void _Main_cs6()
+    {
+      try
+      {
+        var x = new X();
+        WriteLine($"X: {x.Name} {x.Age} | {x}");
+        WriteLine("{0} {1}", nameof(x), nameof(X));
+        X y = null;
+        if (y?.Name?.Length > 0)
+        {
+          WriteLine("Name len is ok");
+        }
+      }
+      catch (Exception ex) { WriteLine($"{ex.GetType().FullName} : {ex.Message}"); }
+    }
+  }
+}
--- Solution1/ConsoleApplication1/Program.cs
+++ Solution1/ConsoleApplication1/Program.cs
@@ -1,54 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using static System.Console;
 
 namespace ConsoleApplication1
 {
-    class X
+  class Program
+  {
+    static void Main()
     {
-        public X()
-        {
-            Name = "uno";
-        }
-        public string Name { get; }
-        public int Age => Name.Length;
-        public override string ToString() => $"({Name},{Age})";
+      //ExploreFeatureCS6_exe._Main_cs6();
+      task1_exe._Main();
     }
-    class Program
-    {
-        static bool log(Exception ex) { WriteLine($"An exception ocurred: {ex.GetType().FullName}"); return false; }
-        static void with_logged_exception(int n)
-        {
-            try
-            {
-                int r = 10 / n;
-            }
-            catch (Exception ex) when (log(ex))
-            {
-                WriteLine("ex is null");
-            }
-        }
-        static void exception_filter()
-        {
-            with_logged_exception(0);
-            WriteLine("Done.");
-        }
-
-        static void Main(string[] args)
-        {
-            try
-            {
-                var x = new X();
-                WriteLine($"X: {x.Name} {x.Age} | {x}");
-                WriteLine("{0} {1}", nameof(x), nameof(X));
-                X y = null;
-                if (y?.Name?.Length > 0)
-                {
-                    WriteLine("Name len is ok");
-                }
-            }
-            catch (Exception ex) { WriteLine($"{ex.GetType().FullName} : {ex.Message}"); }
-        }
-    }
+  }
 }
-//second modified
