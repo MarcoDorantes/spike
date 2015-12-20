4f2c2fa300f512f4796c653e56cd4d47c43604bf
Marco Dorantes <marcodorantes@hotmail.com>
2015-12-20 13:42:54 -0600

    Added j(a)

--- Solution1/ConsoleApplication1/task1.cs
+++ Solution1/ConsoleApplication1/task1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using static System.Console;
 
 interface IA
 {
@@ -7,6 +8,7 @@ interface IA
   Task f0();
   Task f2();
   Task<int> f3();
+  Task<string> GetString();
 }
 
 class A : IA
@@ -28,6 +30,10 @@ class A : IA
   {
     return await _f3();
   }
+  public async Task<string> GetString()
+  {
+    return await Task.Run(()=> { for (var k = 0; k < 3; ++k) System.Threading.Thread.Sleep(600); return "returned string"; });
+  }
 }
 
 class task1_exe
@@ -43,23 +49,48 @@ class task1_exe
   }
   static async void i(IA a)
   {
-    Console.WriteLine($"[{Environment.CurrentManagedThreadId}] i-begin");
+    WriteLine($"[{Environment.CurrentManagedThreadId}] i-begin");
     int r = await a.f3();
-    Console.WriteLine($"[{Environment.CurrentManagedThreadId}] i-result: {r}");
+    WriteLine($"[{Environment.CurrentManagedThreadId}] i-result: {r}");
   }
+  static async Task<string> _j(IA a)
+  {
+    try
+    {
+      WriteLine($"[{Environment.CurrentManagedThreadId}] _j-0");
+      Task<string> task = a.GetString();
+      WriteLine($"[{Environment.CurrentManagedThreadId}] _j-1");
+      string result = await task;
+      WriteLine($"[{Environment.CurrentManagedThreadId}] _j-2");
+      return result;
+    }
+    finally
+    {
+      WriteLine($"[{Environment.CurrentManagedThreadId}] _j-3");
+    }
+  }
+  static async void j(IA a)
+  {
+    WriteLine($"[{Environment.CurrentManagedThreadId}] j-0");
+    Task<string> result = _j(a);
+    WriteLine($"[{Environment.CurrentManagedThreadId}] j-1");
+    WriteLine($"[{Environment.CurrentManagedThreadId}] j-2: { await result }");
+  }
+
   public static void _Main()
   {
     try
     {
-      Console.WriteLine($"[{Environment.CurrentManagedThreadId}] Begin");
+      WriteLine($"[{Environment.CurrentManagedThreadId}] Begin");
       var a = new A();
       g(a);
       h(a);
       i(a);
+      j(a);
 
-      Console.WriteLine($"[{Environment.CurrentManagedThreadId}] Done");
-      Console.ReadLine();
+      WriteLine($"[{Environment.CurrentManagedThreadId}] Done");
+      ReadLine();
     }
-    catch (Exception ex) { Console.WriteLine($"{ex.GetType().FullName}: {ex.Message}"); }
+    catch (Exception ex) { WriteLine($"{ex.GetType().FullName}: {ex.Message}"); }
   }
 }
