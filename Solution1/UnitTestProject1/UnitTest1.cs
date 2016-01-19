f4c8936c3d7f4c9a9ffa7cb04388d2fd9fb31d6d
marcodorantes <marcodorantes@hotmail.com>
2016-01-19 15:27:07 -0600

    Delegate invoke draft.

--- Solution1/UnitTestProject1/UnitTest1.cs
+++ Solution1/UnitTestProject1/UnitTest1.cs
@@ -8,11 +8,26 @@ using static System.Diagnostics.Trace;
 
 namespace UnitTestProject1
 {
+    class A
+    {
+        public event EventHandler<DateTime> OnPulse;
+
+        public void FireSync()
+        {
+            OnPulse?.Invoke(this, DateTime.Now);
+        }
+        public void FireAsync()//System.ArgumentException: The delegate must have only one target.
+        {
+            AsyncCallback callback = EndInvokeFireAsync;
+            OnPulse?.BeginInvoke(this, DateTime.Now, callback , null);
+        }
+        private void EndInvokeFireAsync(IAsyncResult result){}
+    }
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void DefaultDelegateThreading0()
+        public void SyncDelegateThreading0()
         {
             //Arrange
             var queue = new ConcurrentQueue<string>();
@@ -28,14 +43,14 @@ namespace UnitTestProject1
             WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
 
             //Act
-            a.Fire();
+            a.FireSync();
 
             //Assert
             WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
         }
 
         [TestMethod]
-        public void DefaultDelegateThreading()
+        public void SyncDelegateThreading()
         {
             //Arrange
             var queue = new ConcurrentQueue<int>();
@@ -47,20 +62,80 @@ namespace UnitTestProject1
             });
 
             //Act
-            a.Fire();
+            a.FireSync();
 
             //Assert
             queue.ToList().All(n => n == Environment.CurrentManagedThreadId);
         }
-    }
 
-    class A
-    {
-        public event EventHandler<DateTime> OnPulse;
-
-        public void Fire()
+        [TestMethod]
+        public void AsyncDelegateThreading0()
         {
-            OnPulse?.Invoke(this, DateTime.Now);
+            //Arrange
+            var queue = new ConcurrentQueue<string>();
+
+            var a = new A();
+            Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
+            {
+                var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
+                WriteLine(logline);
+                Thread.Sleep(1000);
+                queue.Enqueue(logline);
+            });
+            WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
+
+            //Act
+            try
+            {
+                a.FireAsync();
+                Assert.Fail("Exception not thrown.");
+            } catch (ArgumentException ex)
+            {
+                //Assert
+                Assert.
+                //The delegate must have only one target.
+                     }
         }
+
+
+        /*[TestMethod]
+        public void AsyncDelegateThreading0()
+        {
+            //Arrange
+            var queue = new ConcurrentQueue<string>();
+
+            var a = new A();
+            Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
+            {
+                var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
+                WriteLine(logline);
+                Thread.Sleep(1000);
+                queue.Enqueue(logline);
+            });
+            WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
+
+            //Act
+            a.FireAsync();
+            AlgorithmExecutorDelegate aed = AlgorithmExecutor.Execute;
+            aed.BeginInvoke(_params, t, ExecuteDelegateCompleted, aed);
+
+            //Assert
+            WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
+        }*/
     }
+
+    /*    class GeneralAlgorithm
+        {
+            public virtual void EjecutarDelegado()
+            {
+            }
+        }
+        delegate DateTime AlgorithmExecutorDelegate(DateTime when);
+        static class AlgorithmExecutor
+        {
+            public static DateTime Execute(DateTime when)
+            {
+                return DateTime.Now;
+            }
+        }*/
 }
