5143d9a69391b90d09093c31589f07b9ae3a75fc
marcodorantes <marcodorantes@hotmail.com>
2016-01-20 13:07:56 -0600

    Additional logging.

--- Solution1/UnitTestProject1/UnitTest1.cs
+++ Solution1/UnitTestProject1/UnitTest1.cs
@@ -23,6 +23,10 @@ namespace UnitTestProject1
     }
     private void EndInvokeFireAsync(IAsyncResult result) { }
   }
+  public static class Constant
+  {
+    public const string PrecisionFormat = "yyyy-MM-ddThh-mm-ss.fffffff";
+  }
   [TestClass]
   public class UnitTest1
   {
@@ -35,18 +39,18 @@ namespace UnitTestProject1
       var a = new A();
       Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
       {
-        var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
+        var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e.ToString(Constant.PrecisionFormat)}";
         Trace.WriteLine(logline);
         Thread.Sleep(1000);
         queue.Enqueue(logline);
       });
-      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now.ToString(Constant.PrecisionFormat)}");
 
       //Act
       a.FireEventSync();
 
       //Assert
-      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now.ToString(Constant.PrecisionFormat)}");
       Assert.IsTrue(queue.ToList().All(n => n.StartsWith($"[{Environment.CurrentManagedThreadId}]")));
     }
 
@@ -98,31 +102,81 @@ namespace UnitTestProject1
         whole.Add(new GeneralAlgorithm(next));
         return whole;
       });
-      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now.ToString(Constant.PrecisionFormat)}");
 
       //Act
       list.ForEach(x => x.EjecutarDelegado());
       Thread.Sleep(4000);
 
       //Assert
-      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now.ToString(Constant.PrecisionFormat)}");
       list.ForEach(x => Trace.WriteLine(x.Result));
+      Assert.IsTrue(list.All(n => !n.Result.StartsWith($"[{Environment.CurrentManagedThreadId}]")));
+    }
+    [TestMethod]
+    public void AsyncDelegateThreading2()
+    {
+      //Arrange
+      var x = new GeneralAlgorithm(123);
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now.ToString(Constant.PrecisionFormat)}");
+
+      //Act
+      for (int k = 0; k < 70; ++k)
+      {
+        x.AddResultInTheBackground(k);
+      }
+      Thread.Sleep(4000);
+
+      //Assert
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now.ToString(Constant.PrecisionFormat)}");
+      var results = x.Results.ToList();
+      results.ForEach(_ => Trace.WriteLine(_));
+      Assert.IsTrue(results.All(_ => !_.StartsWith($"[{Environment.CurrentManagedThreadId}]")));
+    }
+    [TestMethod]
+    public void AsyncDelegateThreading3()
+    {
+      //Arrange
+      var x = new GeneralAlgorithm(321);
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now.ToString(Constant.PrecisionFormat)}");
+
+      //Act
+      for (int k = 0; k < 70; ++k)
+      {
+        x.AddResultInTheBackground2(k);
+      }
+      Thread.Sleep(4000);
+
+      //Assert
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now.ToString(Constant.PrecisionFormat)}");
+      var results = x.Results.ToList();
+      results.ForEach(_ => Trace.WriteLine(_));
+      Assert.IsTrue(results.All(_ => !_.StartsWith($"[{Environment.CurrentManagedThreadId}]")));
     }
   }
   class GeneralAlgorithm
   {
     private AlgorithmExecutorDelegate aed;
+    private AddResultInTheBackground_Delegate AddResultInTheBackground_Work;
+    private AddResultInTheBackground_Delegate AddResultInTheBackground_Work2;
+    private AlgorithmExecutor2 algorithm;
+
     public GeneralAlgorithm(int n)
     {
+      algorithm = new AlgorithmExecutor2();
       ID = n;
+      Results = new ConcurrentQueue<string>();
       aed = AlgorithmExecutor.Execute;
+      AddResultInTheBackground_Work = AlgorithmExecutor.Execute;
+      AddResultInTheBackground_Work2 = algorithm.Execute;
     }
     public int ID;
     public string Result;
+    public ConcurrentQueue<string> Results;
     public virtual void EjecutarDelegado()
     {
       AsyncCallback callback = ExecuteDelegateCompleted;
-      aed.BeginInvoke($"([{Environment.CurrentManagedThreadId}] BeginInvoke {ID} at {DateTime.Now:s})", callback, null);
+      aed.BeginInvoke($"([{Environment.CurrentManagedThreadId}] BeginInvoke {ID} at {DateTime.Now.ToString(Constant.PrecisionFormat)})", callback, null);
     }
     private void ExecuteDelegateCompleted(IAsyncResult result)
     {
@@ -135,13 +189,55 @@ namespace UnitTestProject1
         Result = $"{ex.GetType().FullName}: {ex.Message}";
       }
     }
+
+    public void AddResultInTheBackground(int input)
+    {
+      AddResultInTheBackground_Work.BeginInvoke($"([{Environment.CurrentManagedThreadId}] BeginInvoke {ID} with input {input} at {DateTime.Now.ToString(Constant.PrecisionFormat)})", AddResultInTheBackground_Callback, input);
+    }
+    private void AddResultInTheBackground_Callback(IAsyncResult result)
+    {
+      try
+      {
+        var entry = $"[{Environment.CurrentManagedThreadId}] callback at {DateTime.Now.ToString(Constant.PrecisionFormat)}";
+        Results.Enqueue($"{entry} | {AddResultInTheBackground_Work.EndInvoke(result)} | {result.AsyncState}");
+      }
+      catch (Exception ex)
+      {
+        Results.Enqueue($"{ex.GetType().FullName}: {ex.Message}");
+      }
+    }
+
+    public void AddResultInTheBackground2(int input)
+    {
+      AddResultInTheBackground_Work2.BeginInvoke($"([{Environment.CurrentManagedThreadId}] BeginInvoke {ID} with input {input} at {DateTime.Now.ToString(Constant.PrecisionFormat)})", AddResultInTheBackground_Callback2, input);
+    }
+    private void AddResultInTheBackground_Callback2(IAsyncResult result)
+    {
+      try
+      {
+        Results.Enqueue($"{AddResultInTheBackground_Work2.EndInvoke(result)} | {result.AsyncState}");
+      }
+      catch (Exception ex)
+      {
+        Results.Enqueue($"{ex.GetType().FullName}: {ex.Message}");
+      }
+    }
   }
   delegate string AlgorithmExecutorDelegate(string when);
+  delegate string AddResultInTheBackground_Delegate(string input);
   static class AlgorithmExecutor
   {
     public static string Execute(string when)
     {
-      return $"[{Environment.CurrentManagedThreadId}] return: {DateTime.Now:s} {when}";
+      return $"[{Environment.CurrentManagedThreadId}] return: {DateTime.Now.ToString(Constant.PrecisionFormat)} {when}";
     }
   }
+  class AlgorithmExecutor2
+  {
+    public string Execute(string when)
+    {
+      return $"[{Environment.CurrentManagedThreadId}] return: {DateTime.Now.ToString(Constant.PrecisionFormat)} {when}";
+    }
+  }
+
 }
