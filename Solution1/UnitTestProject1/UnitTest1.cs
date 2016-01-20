b55db313889fe1a092e64c6e80f1455f7088ab73
Marco Dorantes <marcodorantes@hotmail.com>
2016-01-19 22:13:45 -0600

    Tried AsyncDelegateThreading1

--- Solution1/UnitTestProject1/UnitTest1.cs
+++ Solution1/UnitTestProject1/UnitTest1.cs
@@ -3,139 +3,145 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Threading;
 using System.Collections.Concurrent;
-
-using static System.Diagnostics.Trace;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
-    class A
-    {
-        public event EventHandler<DateTime> OnPulse;
+  class A
+  {
+    public event EventHandler<DateTime> OnPulse;
 
-        public void FireSync()
-        {
-            OnPulse?.Invoke(this, DateTime.Now);
-        }
-        public void FireAsync()//System.ArgumentException: The delegate must have only one target.
-        {
-            AsyncCallback callback = EndInvokeFireAsync;
-            OnPulse?.BeginInvoke(this, DateTime.Now, callback , null);
-        }
-        private void EndInvokeFireAsync(IAsyncResult result){}
+    public void FireEventSync()
+    {
+      OnPulse?.Invoke(this, DateTime.Now);
     }
-    [TestClass]
-    public class UnitTest1
+    public void FireEventAsync()
     {
-        [TestMethod]
-        public void SyncDelegateThreading0()
-        {
-            //Arrange
-            var queue = new ConcurrentQueue<string>();
+      AsyncCallback callback = EndInvokeFireAsync;
+      OnPulse?.BeginInvoke(this, DateTime.Now, callback, null);
+    }
+    private void EndInvokeFireAsync(IAsyncResult result) { }
+  }
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void SyncDelegateThreading0()
+    {
+      //Arrange
+      var queue = new ConcurrentQueue<string>();
 
-            var a = new A();
-            Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
-            {
-                var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
-                WriteLine(logline);
-                Thread.Sleep(1000);
-                queue.Enqueue(logline);
-            });
-            WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
+      var a = new A();
+      Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
+      {
+        var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
+        Trace.WriteLine(logline);
+        Thread.Sleep(1000);
+        queue.Enqueue(logline);
+      });
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
 
-            //Act
-            a.FireSync();
+      //Act
+      a.FireEventSync();
 
-            //Assert
-            WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
-        }
-
-        [TestMethod]
-        public void SyncDelegateThreading()
-        {
-            //Arrange
-            var queue = new ConcurrentQueue<int>();
-
-            var a = new A();
-            Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
-            {
-                queue.Enqueue(Environment.CurrentManagedThreadId);
-            });
-
-            //Act
-            a.FireSync();
-
-            //Assert
-            queue.ToList().All(n => n == Environment.CurrentManagedThreadId);
-        }
-
-        [TestMethod]
-        public void AsyncDelegateThreading0()
-        {
-            //Arrange
-            var queue = new ConcurrentQueue<string>();
-
-            var a = new A();
-            Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
-            {
-                var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
-                WriteLine(logline);
-                Thread.Sleep(1000);
-                queue.Enqueue(logline);
-            });
-            WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
-
-            //Act
-            try
-            {
-                a.FireAsync();
-                Assert.Fail("Exception not thrown.");
-            } catch (ArgumentException ex)
-            {
-                //Assert
-                Assert.
-                //The delegate must have only one target.
-                     }
-        }
-
-
-        /*[TestMethod]
-        public void AsyncDelegateThreading0()
-        {
-            //Arrange
-            var queue = new ConcurrentQueue<string>();
-
-            var a = new A();
-            Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
-            {
-                var logline = $"[{Environment.CurrentManagedThreadId}] {n} {e:s}";
-                WriteLine(logline);
-                Thread.Sleep(1000);
-                queue.Enqueue(logline);
-            });
-            WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
-
-            //Act
-            a.FireAsync();
-            AlgorithmExecutorDelegate aed = AlgorithmExecutor.Execute;
-            aed.BeginInvoke(_params, t, ExecuteDelegateCompleted, aed);
-
-            //Assert
-            WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
-        }*/
+      //Assert
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
+      Assert.IsTrue(queue.ToList().All(n => n.StartsWith($"[{Environment.CurrentManagedThreadId}]")));
     }
 
-    /*    class GeneralAlgorithm
-        {
-            public virtual void EjecutarDelegado()
-            {
-            }
-        }
-        delegate DateTime AlgorithmExecutorDelegate(DateTime when);
-        static class AlgorithmExecutor
-        {
-            public static DateTime Execute(DateTime when)
-            {
-                return DateTime.Now;
-            }
-        }*/
+    [TestMethod]
+    public void SyncDelegateThreading()
+    {
+      //Arrange
+      var queue = new ConcurrentQueue<int>();
+
+      var a = new A();
+      Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) =>
+      {
+        queue.Enqueue(Environment.CurrentManagedThreadId);
+      });
+
+      //Act
+      a.FireEventSync();
+
+      //Assert
+      Assert.IsTrue(queue.ToList().All(n => n == Environment.CurrentManagedThreadId));
+    }
+
+    [TestMethod]
+    public void AsyncDelegateThreading0()
+    {
+      //Arrange
+      var a = new A();
+      Enumerable.Range(1, 10).ToList().ForEach(n => a.OnPulse += (s, e) => { });
+
+      //Act
+      try
+      {
+        a.FireEventAsync();
+        Assert.Fail("Exception not thrown.");
+      }
+      catch (ArgumentException ex)
+      {
+        Assert.AreEqual<string>("The delegate must have only one target.", ex.Message);
+      }
+    }
+
+    [TestMethod]
+    public void AsyncDelegateThreading1()
+    {
+      //Arrange
+      var list = new List<GeneralAlgorithm>();
+      Enumerable.Range(1, 10).Aggregate(list,(whole, next) =>
+      {
+        whole.Add(new GeneralAlgorithm(next));
+        return whole;
+      });
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Arrange {DateTime.Now:s}");
+
+      //Act
+      list.ForEach(x => x.EjecutarDelegado());
+      Thread.Sleep(4000);
+
+      //Assert
+      Trace.WriteLine($"[{Environment.CurrentManagedThreadId}] Assert {DateTime.Now:s}");
+      list.ForEach(x => Trace.WriteLine(x.Result));
+    }
+  }
+  class GeneralAlgorithm
+  {
+    private AlgorithmExecutorDelegate aed;
+    public GeneralAlgorithm(int n)
+    {
+      ID = n;
+      aed = AlgorithmExecutor.Execute;
+    }
+    public int ID;
+    public string Result;
+    public virtual void EjecutarDelegado()
+    {
+      AsyncCallback callback = ExecuteDelegateCompleted;
+      aed.BeginInvoke($"([{Environment.CurrentManagedThreadId}] BeginInvoke {ID} at {DateTime.Now:s})", callback, null);
+    }
+    private void ExecuteDelegateCompleted(IAsyncResult result)
+    {
+      try
+      {
+        Result = aed.EndInvoke(result);
+      }
+      catch (Exception ex)
+      {
+        Result = $"{ex.GetType().FullName}: {ex.Message}";
+      }
+    }
+  }
+  delegate string AlgorithmExecutorDelegate(string when);
+  static class AlgorithmExecutor
+  {
+    public static string Execute(string when)
+    {
+      return $"[{Environment.CurrentManagedThreadId}] return: {DateTime.Now:s} {when}";
+    }
+  }
 }
