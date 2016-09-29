dfcf337f6afd3b7cd97d84d6c6c99554574df2f7
Marco Dorantes <marcodorantes@hotmail.com>
2016-09-29 06:41:11 -0500

    Added doc_rpc_call_spec

---
+++ Solution1/UnitTestProject1/doc_rpc_call_spec.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class doc_rpc_call_spec
+  {
+    //http://stackoverflow.com/questions/9062475/what-is-the-difference-between-document-style-and-rpc-style-communication
+    //http://java.globinch.com/enterprise-java/web-services/soap-binding-document-rpc-style-web-services-difference/
+    //https://msdn.microsoft.com/en-us/library/4cxy91t2(v=vs.100).aspx
+    //https://msdn.microsoft.com/en-us/library/ms996466.aspx
+    //https://msdn.microsoft.com/en-us/library/ms996486.aspx
+    [TestMethod]
+    public void a_xml_call()
+    {
+      var xml = new XDocument(
+        new XElement("proc1",
+          new XElement("arg1","val1")
+        )
+      );
+
+      Assert.AreEqual<string>("<proc1><arg1>val1</arg1></proc1>", xml.ToString(SaveOptions.DisableFormatting));
+    }
+
+    [TestMethod]
+    public void a_xml_call_2()
+    {
+      var xml = new XDocument(
+        new XElement("proc",
+          new XElement("name", "proc1"),
+          new XElement("args",
+            new XElement("arg", new XElement("name", "name1"), new XElement("value", "val1"))
+          )
+        )
+      );
+
+      Assert.AreEqual<string>("<proc1><arg1>val1</arg1></proc1>", xml.ToString(SaveOptions.DisableFormatting));
+    }
+  }
+}
