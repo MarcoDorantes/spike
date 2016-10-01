e851354d68d83f18a0dac7382bcac30221fe7790
Marco Dorantes <marcodorantes@hotmail.com>
2016-10-01 12:16:11 -0500

    Added regions and minor clean up.

--- Solution1/UnitTestProject1/doc_rpc_call_spec.cs
+++ Solution1/UnitTestProject1/doc_rpc_call_spec.cs
@@ -9,13 +9,17 @@ using nutility;
 namespace UnitTestProject1
 {
   [TestClass]
-  public class doc_rpc_call_spec
+  public class RPC_As_An_AbstractDataType_Spec
   {
+    #region SOAP
     //http://stackoverflow.com/questions/9062475/what-is-the-difference-between-document-style-and-rpc-style-communication
     //http://java.globinch.com/enterprise-java/web-services/soap-binding-document-rpc-style-web-services-difference/
     //https://msdn.microsoft.com/en-us/library/4cxy91t2(v=vs.100).aspx
     //https://msdn.microsoft.com/en-us/library/ms996466.aspx
     //https://msdn.microsoft.com/en-us/library/ms996486.aspx
+    #endregion
+
+    #region XML data type cases
     [TestMethod]
     public void a_xml_call()
     {
@@ -42,22 +46,24 @@ namespace UnitTestProject1
 
       Assert.AreEqual<string>("<proc><name>proc1</name><args><arg><name>name1</name><value>val1</value></arg></args></proc>", xml.ToString(SaveOptions.DisableFormatting));
     }
+    #endregion
 
+    #region GetStoredProcedureCall as a first direct try
     interface IOperationCallBuilder
     {
       string GetRunStatement();
       string GetOperationName();
-      string RenderArguments();
+      string GetArguments();
+    }
+    static string GetStoredProcedureCall(nutility.ITypeClassMapper typemap)
+    {
+      var builder = typemap.GetService<IOperationCallBuilder>();
+      return $"{builder.GetRunStatement()} {builder.GetOperationName()} {builder.GetArguments()}";
     }
     interface IParameterMetadataProvider
     {
       void LoadSchemaFor(string operation_name);
       string GetLiteralValue(string parameter_name, string parameter_value);
-    }
-    static string GetStoredProcedureCall(nutility.ITypeClassMapper typemap)
-    {
-      var builder = typemap.GetService<IOperationCallBuilder>();
-      return $"{builder.GetRunStatement()} {builder.GetOperationName()} {builder.RenderArguments()}";
     }
     class OperationCallBuilder : IOperationCallBuilder
     {
@@ -77,7 +83,7 @@ namespace UnitTestProject1
       {
         return xml.Root.Name.LocalName;
       }
-      public string RenderArguments()
+      public string GetArguments()
       {
         if (xml.Root.Elements().Count() > 0)
         {
@@ -113,7 +119,7 @@ namespace UnitTestProject1
       }
     }
 
-    [TestMethod]
+    [TestMethod, Description("No schema data")]
     public void a_xml_call_3()
     {
       var xml = XDocument.Parse("<sp1><par1>val1</par1><par2>123.45</par2></sp1>");
@@ -133,42 +139,55 @@ namespace UnitTestProject1
       var tsql = GetStoredProcedureCall(typemap);
       Assert.AreEqual<string>("EXECUTE sp1 @par1 = 'val1', @par2 = '123.45'", tsql);
     }
-    [TestMethod]
+
+    [TestMethod, Description("Added simple schema data")]
     public void a_xml_call_4()
     {
       var xml = XDocument.Parse("<sp1><par1>val1</par1><par2>123.45</par2></sp1>");
       var schema = new Dictionary<string, Type> { { "par2", typeof(decimal) } };
       var typemap = new nutility.TypeClassMapper
       (
-        new Dictionary<Type, Type> {
+        new Dictionary<Type, Type>
+        {
           { typeof(IOperationCallBuilder), typeof(OperationCallBuilder) },
           { typeof(IParameterMetadataProvider), typeof(ParameterMetadataProvider) }
         },
-        new Dictionary<Type, object> {
-          { typeof(IDictionary<string, Type>), schema },
+        new Dictionary<Type, object>
+        {
           { typeof(XDocument), xml},
+          { typeof(IDictionary<string, Type>), schema }
         }
       );
 
       var tsql = GetStoredProcedureCall(typemap);
       Assert.AreEqual<string>("EXECUTE sp1 @par1 = 'val1', @par2 = 123.45", tsql);
     }
-    interface IDataType
+    #endregion
+
+    #region An inbound XML message processing
+    interface RPC_As_AbstractDataType
     {
       void SetSerializedDataTypeAsXDocument(XDocument xml);
       XDocument GetXDocument();
+
+      //string GetRunStatement();
+      //string GetOperationName();
+      //string GetArguments();
     }
-    interface IDataTypeToOperationExecution { string GetOperationExecution(IDataType data, nutility.ITypeClassMapper typemap); }
+    interface IDataTypeToOperationExecution
+    {
+      string GetOperationExecution(RPC_As_AbstractDataType data, nutility.ITypeClassMapper typemap);
+    }
     class DataTypeToStoredProcedureCall : IDataTypeToOperationExecution
     {
-      public string GetOperationExecution(IDataType data, ITypeClassMapper typemap)
+      public string GetOperationExecution(RPC_As_AbstractDataType data, ITypeClassMapper typemap)
       {
         typemap.AddMapping<XDocument>(data.GetXDocument());
         var tsql = GetStoredProcedureCall(typemap);
         return tsql;
       }
     }
-    class DataType : IDataType
+    class ProcedureCallAsXmlMessage : RPC_As_AbstractDataType
     {
       private XDocument xml;
 
@@ -186,7 +205,7 @@ namespace UnitTestProject1
         {
           { typeof(IOperationCallBuilder), typeof(OperationCallBuilder) },
           { typeof(IDataTypeToOperationExecution), typeof(DataTypeToStoredProcedureCall) },
-          { typeof(IDataType), typeof(DataType) },
+          { typeof(RPC_As_AbstractDataType), typeof(ProcedureCallAsXmlMessage) },
           { typeof(IParameterMetadataProvider), typeof(ParameterMetadataProvider) }
         },
         new Dictionary<Type, object>
@@ -194,11 +213,12 @@ namespace UnitTestProject1
           { typeof(IDictionary<string, Type>), schema }
         }
       );
-      var inbound = typemap.GetService<IDataType>();
+      var inbound = typemap.GetService<RPC_As_AbstractDataType>();
       inbound.SetSerializedDataTypeAsXDocument(xml);
       var transform = typemap.GetService<IDataTypeToOperationExecution>();
       var execution = transform.GetOperationExecution(inbound, typemap);
       Assert.AreEqual<string>("EXECUTE sp1 @par1 = 'val1', @par2 = 123.45", execution);
     }
+    #endregion
   }
 }
