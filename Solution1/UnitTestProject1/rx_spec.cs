0d76a7147232e2ea14e0f9d516829e95565db88d
Marco Dorantes <marcodorantes@hotmail.com>
2017-01-06 17:56:03 -0600

    Added RX_Spec

---
+++ Solution1/UnitTestProject1/rx_spec.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class RX_Spec
+  {
+    /*
+https://github.com/Reactive-Extensions/Rx.NET
+https://msdn.microsoft.com/en-us/library/hh242985(v=vs.103).aspx
+https://msdn.microsoft.com/en-us/library/system.reactive.linq.observable(v=vs.103).aspx
+     */
+    [TestMethod]
+    public void TestMethod1()
+    {
+      //System.Reactive.dll
+      //Observable.
+      Assert.IsTrue(this.GetType().Assembly.GetReferencedAssemblies().Any(a => a.FullName.StartsWith("System.Reactive")));
+    }
+  }
+}
