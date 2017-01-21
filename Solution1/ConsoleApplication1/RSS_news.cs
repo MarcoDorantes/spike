6d34c5e20ec0588a45b65ed04e2f79e20128aec6
Marco Dorantes <marcodorantes@hotmail.com>
2017-01-21 12:53:54 -0600

    Added show all links to RSS_news

--- Solution1/ConsoleApplication1/RSS_news.cs
+++ Solution1/ConsoleApplication1/RSS_news.cs
@@ -31,25 +31,34 @@ namespace ConsoleApplication1
     {
       try
       {
-        string sourceURL = "http://sxp.microsoft.com/feeds/msdntn/VisualStudioNews";
         string targetfile = $"{Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0])}_target.xml";
+        if (args.Length == 0)
+        {
+          string sourceURL = "http://sxp.microsoft.com/feeds/msdntn/VisualStudioNews";
 
-        //if (args.Length != 1) throw new Exception("No source file specified.");
-        //var sourcefile = args[0];
-        //if (!File.Exists(sourcefile)) throw new Exception($"No source file found: {sourcefile}");
-        var source = XDocument.Load(sourceURL);
-        WriteLine($"Source text received: {source}");
-        if (!File.Exists(targetfile)) throw new Exception($"No target file found: {targetfile}");
-        var target = XDocument.Load(targetfile);
-        Write($"Target file: {targetfile}...");
-        var m = new guid_based_comparer();
-        WriteLine("\n");
-        var merged = m.merge(source, target);
-        merged.Save(targetfile);
-        WriteLine("merged.");
-        var next = @"tools\windiff afile_copy.xml vsnews_merge_target.xml";
-        System.Windows.Clipboard.SetText(next);
-        WriteLine(next);
+          //if (args.Length != 1) throw new Exception("No source file specified.");
+          //var sourcefile = args[0];
+          //if (!File.Exists(sourcefile)) throw new Exception($"No source file found: {sourcefile}");
+          var source = XDocument.Load(sourceURL);
+          WriteLine($"Source text received: {source}");
+          if (!File.Exists(targetfile)) throw new Exception($"No target file found: {targetfile}");
+          var target = XDocument.Load(targetfile);
+          Write($"Target file: {targetfile}...");
+          var m = new guid_based_comparer();
+          WriteLine("\n");
+          var merged = m.merge(source, target);
+          merged.Save(targetfile);
+          WriteLine("merged.");
+          var next = @"tools\windiff vsnews_merge_target.xml afile_copy.xml";
+          System.Windows.Clipboard.SetText(next);
+          WriteLine(next);
+        }
+        else
+        {
+          var target = XDocument.Load(targetfile);
+          WriteLine($"Links in target file: {targetfile}...");
+          target.Root.Descendants("item").ToList().ForEach(n => { WriteLine($"{n.Element("link").Value}\t{n.Element("title").Value}"); });
+        }
       }
       catch (Exception ex) { WriteLine($"{ex.GetType().FullName}: {ex.Message}"); }
     }
