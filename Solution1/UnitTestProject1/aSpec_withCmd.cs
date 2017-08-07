0976dfb215eb83d4cdc96e12018ae33007f7231b
Marco Dorantes <marcodorantes@hotmail.com>
2017-08-07 03:50:57 -0500

    Added WriterLogCopy.Copy method.

--- Solution1/UnitTestProject1/aSpec_withCmd.cs
+++ Solution1/UnitTestProject1/aSpec_withCmd.cs
@@ -9,10 +9,12 @@ namespace UnitTestProject1
 {
     static class WriterLogCopy
     {
+        const int TimeoutAfter = 20 * 60 * 60 * 1000; // 20 minutes
+
         public static string GetSourceFilePattern(string filename_start, string source_folder, DateTime? date = null)
         {
             var when = date.HasValue ? date.Value : DateTime.Now.Date;
-            return $"{filename_start}_{DateTime.Now.ToString("MMMdd")}*.log";
+            return $"{filename_start}_{when.ToString("MMMdd")}*.log";
         }
         //public static FileInfo GetFileInfoFor(string filename_start, string source_folder, DateTime? date = null)
         //{
@@ -20,6 +22,39 @@ namespace UnitTestProject1
         //    var path = Path.Combine(source_folder, $"{filename_start}_{DateTime.Now.ToString("MMMdd")}*.log");
         //    return new FileInfo(path);
         //}
+        public static IDictionary<FileInfo, KeyValuePair<FileInfo, Process>> Copy(string[][] sources, string target_folder, DateTime? date = null)
+        {
+            var copied_sources = new Dictionary<FileInfo, KeyValuePair<FileInfo, Process>>();
+            foreach (var pair in sources)
+            {
+                string filename_start = pair[0];
+                string source_folder = pair[1];
+                Trace.WriteLine($"To search for {filename_start} at {source_folder}.");
+                var sourceSearchPattern = GetSourceFilePattern(filename_start, source_folder, date);
+
+                Trace.WriteLine($"\tSearching for {sourceSearchPattern}...");
+                var from_folder = new DirectoryInfo(source_folder);
+                foreach (var sourcefile in from_folder.GetFiles(sourceSearchPattern))
+                {
+                    Trace.WriteLine($"\tCopying {source_folder} {target_folder} {sourceSearchPattern}...");
+                    var startinfo = new ProcessStartInfo("cmd.exe", $"/c robocopy {source_folder} {target_folder} {sourceSearchPattern}");
+                    startinfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startinfo.UseShellExecute = false;
+                    startinfo.RedirectStandardError = true;
+                    startinfo.RedirectStandardOutput = true;
+                    var p = new Process();
+                    p.StartInfo = startinfo;
+                    p.Start();
+                    string output = p.StandardOutput.ReadToEnd(); //https://msdn.microsoft.com/en-us/library/system.diagnostics.process.standardoutput(v=vs.110).aspx
+                    bool exited = p.WaitForExit(TimeoutAfter);
+                    Trace.WriteLine($"\tCopy exited: {exited} ExitCode: {p.ExitCode} Output: {output}");
+
+                    var target = new FileInfo(@"C:\some");
+                    copied_sources.Add(sourcefile, new KeyValuePair<FileInfo, Process>(target, p));
+                }
+            }
+            return copied_sources;
+        }
     }
     /*
 GBMSOLACEWIN02
@@ -50,7 +85,7 @@ FixWriter5
 
             var s = new DirectoryInfo(source_folder);
             var sourcefiles = s.GetFiles(sourceSearchPattern).ToList();
-            Assert.AreEqual<int>(0,sourcefiles.Count);
+            Assert.AreEqual<int>(0, sourcefiles.Count);
         }
         [TestMethod]
         public void copy0()
@@ -67,21 +102,42 @@ FixWriter5
                 var from_folder = new DirectoryInfo(source_folder);
                 foreach (var sourcefile in from_folder.GetFiles(sourceSearchPattern))
                 {
-                    var startinfo = new ProcessStartInfo("cmd.exe",$"/c robocopy {source_folder} {target_folder} {sourceSearchPattern}");
+                    var startinfo = new ProcessStartInfo("cmd.exe", $"/c robocopy {source_folder} {target_folder} {sourceSearchPattern}");
                     startinfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startinfo.UseShellExecute = false;
+                    startinfo.RedirectStandardError = true;
+                    startinfo.RedirectStandardOutput = true;
                     var p = new Process();
                     p.StartInfo = startinfo;
                     p.Start();
+                    string output = p.StandardOutput.ReadToEnd();
                     bool exited = p.WaitForExit(timeout_after);
                     Assert.IsTrue(exited);
                     Assert.IsTrue(p.ExitCode == 0 || p.ExitCode == 1);//https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
+                    Trace.WriteLine(output);
                     copied_sources.Add(sourcefile, p);
                 }
                 Assert.AreEqual<int>(1, copied_sources.Count);
             }
-            finally
+            finally{}
+        }
+        [TestMethod]
+        public void copy1()
+        {
+            try
             {
+                var target_folder = @"C:\Users\Marco\AppData\Local\temp\target";
+
+                string[][] sources = {
+                    new[] { "FixWriterFolioService1", @"\\HOMESTATION2\C$\Users\Marco\AppData\Local\temp\source1" },
+                    new[] { "FixWriterVirtuDCService1", @"\\HOMESTATION2\C$\Users\Marco\AppData\Local\temp\source2" },
+//                    new[] { "FixWriterVirtuDCService2", @"\\HOMESTATION2\C$\Users\Marco\AppData\Local\temp\source3" }
+                };
+                var copied = WriterLogCopy.Copy(sources, target_folder, new DateTime(2017, 8, 6));
+
+                foreach (var copy in copied.Keys) Trace.WriteLine($"\nCopy: {copy.FullName}");
             }
+            finally { }
         }
     }
 }
