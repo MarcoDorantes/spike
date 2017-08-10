442e9844f982c7355767b9220022cd409411e6eb
marcodorantes <marcodorantes@hotmail.com>
2017-08-10 17:41:29 -0500

    Added NetCoreConsoleApp1

---
+++ Solution1/NetCoreConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace NetCoreConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            System.Threading.Thread.Sleep(450);
+            watch.Stop();
+            WriteLine($"watch: {watch.IsRunning} {watch.ElapsedMilliseconds}");
+        }
+    }
+}
