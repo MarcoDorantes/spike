2ca525695c18f1dc5aea146019ac0b71a95af262
marcodorantes <marcodorantes@hotmail.com>
2017-08-24 14:42:19 -0500

    Added projects to .NET Core 2.0 solution folder: ASP_NET_Core2_MVC_App1 ASP_NET_Core2_WebAPI_1 and the like.

---
+++ Solution1/ASP_NET_Core2_MVC_App1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_NET_Core2_MVC_App1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Solution1/NETCore2ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NETCore2ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
