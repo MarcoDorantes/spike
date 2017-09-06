5ad60b5e3fb98f6f831522fbad08c21e9146e212
marcodorantes <marcodorantes@hotmail.com>
2017-09-06 10:10:27 -0500

    Added SignalRConsoleClient1 and SignalRConsoleServer1.

---
+++ Solution1/SignalRConsoleClient1/ClientProgram.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+using static System.Console;
+using System.Net.Http;
+
+namespace SignalRConsoleClient1
+{
+    class ClientProgram
+    {
+        const string ServerURI = "http://localhost:8080/signalr";
+        static HubConnection Connection;
+        static IHubProxy HubProxy;
+
+        /// <summary> 
+        /// Creates and connects the hub connection and hub proxy. This method 
+        /// is called asynchronously from SignInButton_Click. 
+        /// </summary> 
+        private static async void ConnectAsync()
+        {
+            Connection = new HubConnection(ServerURI);
+            Connection.Closed += () => WriteLine("You have been disconnected.");
+            HubProxy = Connection.CreateHubProxy("MyHub");
+            //Handle incoming event from server: use Invoke to write to console from SignalR's thread 
+            HubProxy.On<string, string>("AddMessage", (name, message) => WriteLine($"{name}: {message}"));
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException)
+            {
+                WriteLine("Unable to connect to server: Start server before connecting clients.");
+                //No connection: Don't enable Send button or show chat UI 
+                return;
+            }
+            WriteLine("Connected to server at " + ServerURI + "\r");
+        }
+        static void start(string[] args)
+        {
+            WriteLine("Connecting to server...");
+            ConnectAsync();
+        }
+        static void send()
+        {
+            HubProxy.Invoke("Send", $"{System.Diagnostics.Process.GetCurrentProcess().Id}", $"{DateTime.Now.ToString("s")}");
+        }
+        static void stop()
+        {
+            Connection?.Stop();
+            Connection?.Dispose();
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                start(args);
+                WriteLine("Press ENTER to send"); ReadLine();
+                send();
+                WriteLine("Press ENTER to exit"); ReadLine();
+                stop();
+            }
+            catch (Exception ex) { WriteLine($"{ex.GetType().FullName}: {ex.Message}"); }
+        }
+    }
+}
---
+++ Solution1/SignalRConsoleServer1/ServerProgram.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Hosting;
+
+using static System.Console;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRConsoleServer1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    class ServerProgram
+    {
+        static IDisposable SignalR;
+        static void start(string[] args)
+        {
+            const string ServerURI = "http://localhost:8080";
+            SignalR = WebApp.Start(ServerURI);
+        }
+        static void stop() { SignalR.Dispose(); }
+        static void Main(string[] args)
+        {
+            try
+            {
+                start(args);
+                WriteLine("Press ENTER to exit"); ReadLine();
+                stop();
+            }
+            catch (Exception ex) { WriteLine($"{ex.GetType().FullName}: {ex.Message}"); }
+        }
+    }
+
+    /// <summary> 
+    /// Echoes messages sent using the Send message by calling the 
+    /// addMessage method on the client. Also reports to the console 
+    /// when clients connect and disconnect. 
+    /// </summary> 
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+        public override Task OnConnected()
+        {
+            WriteLine("Client connected: " + Context.ConnectionId);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            WriteLine("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
