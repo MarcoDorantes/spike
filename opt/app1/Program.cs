1910dbe86670388e306e4d06c642ac4dfbc34109
marcodorantes <marcodorantes@hotmail.com>
2018-10-04 12:58:40 -0500

    Added app1.

---
+++ opt/app1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace app1
+{
+    class Input
+    {
+        public void op1()
+        {
+            WriteLine($"This is {nameof(op1)}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WriteLine($"Count:{args.Length}"); for (int k = 0; k < args.Length; ++k) WriteLine($"{k}:[{args[k]}]");
+                if (args.Length > 0)
+                {
+                    utility.Switch.AsType<Input>(args);
+                }
+                else
+                {
+                    utility.Switch.ShowUsage(typeof(Input));
+                }
+            }
+            catch (Exception ex)
+            {
+                for (int level = 0; ex != null; ex = ex?.InnerException, ++level)
+                {
+                    WriteLine($"[{level}] {ex.GetType().FullName}: {ex.Message}");
+                }
+            }
+        }
+    }
+}
