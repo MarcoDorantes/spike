6dec44b5ddae834c166f85af42ae51eb52abd5ee
MarcoDorantes <marcodorantes@hotmail.com>
2023-06-29 17:27:57 -0600

    Updated source/globalized/prog1.cpp

--- source/globalized/prog1.cpp
+++ source/globalized/prog1.cpp
@@ -1,5 +1,4 @@
-//cl /EHsc .\prog1.cpp
-
+//cl /EHsc -std:c++20 .\prog1.cpp
 #include <iostream>
 #include <locale>
 
@@ -9,7 +8,12 @@
 #include <cwchar>
 #include <iterator>
 #include <string>
- 
+
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
 void loc1()//https://en.cppreference.com/w/cpp/locale/setlocale
 {
     // Make a "deep copy" of current locale name.
@@ -65,12 +69,58 @@ void globalized()//https://en.cppreference.com/w/cpp/locale/locale
     std::wcout << 1000.01 << '\n';
 }
 
-int main() {
+void show_status(const fs::path& p, fs::file_status s)//https://en.cppreference.com/w/cpp/filesystem/directory_entry/status
+{
+    std::cout << p;
+    // alternative: switch(s.type()) { case fs::file_type::regular: ...}
+    if (fs::is_regular_file(s)) std::cout << " is a regular file\n";
+    if (fs::is_directory(s)) std::cout << " is a directory\n";
+    if (fs::is_block_file(s)) std::cout << " is a block device\n";
+    if (fs::is_character_file(s)) std::cout << " is a character device\n";
+    if (fs::is_fifo(s)) std::cout << " is a named IPC pipe\n";
+    if (fs::is_socket(s)) std::cout << " is a named IPC socket\n";
+    if (fs::is_symlink(s)) std::cout << " is a symlink\n";
+    if (!fs::exists(s)) std::cout << " does not exist\n";
+}
+
+void read1(int argc, char** argv)
+{
+	if (argc != 2) return;
+	auto filename = argv[1];
+	std::cout << "Reading " << filename << std::endl;
+	std::filesystem::directory_entry entry{filename};
+    auto ok = entry.exists();
+	std::cout << "Exists " << ok << std::endl;
+    std::cout << "Status:" << std::endl;
+    show_status(filename, fs::status(filename));
+
+    //if (const char* loc = std::setlocale(LC_ALL, ".1252")){}
+
+    std::ifstream file;
+
+    auto loc = std::locale(".1252");
+    file.imbue(loc);
+    std::wcout.imbue(loc);
+
+    file.open(filename);
+    std::cout << "File open status:" << file.is_open() << std::endl;
+
+    for (std::string line; getline(file, line); )
+    {
+        std::cout << line;
+    }
+
+    file.close();
+}
+
+int main(int argc, char** argv)
+{
   std::cout << "Hello World!\n";
 
   globalized();
   loc1();
   loc2();
+  read1(argc, argv);
 
   return 0;
 }
