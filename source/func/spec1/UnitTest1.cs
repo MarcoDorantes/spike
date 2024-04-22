927c1628072212102a9b6ac9e991bfa8451db664
MarcoDorantes <marcodorantes@hotmail.com>
2024-04-22 11:27:33 -0600

    Added source\func

---
+++ source/func/spec1/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace spec1;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ source/func/spec2/Tests.fs
@@ -0,0 +1,11 @@
+namespace spec2
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
