d8c2a1ca38feb981f037d98240870aac7be47563
MarcoDorantes <marcodorantes@hotmail.com>
2024-06-08 14:20:58 -0600

    Added source/MSEdgeBookmarks

---
+++ source/MSEdgeBookmarks/when.js
@@ -0,0 +1,5 @@
+//var timeValue = '13285408186887489';
+//var timeValue = '13228215968000000';
+var timeValue = '13362347413406690';
+var w=new Date(Date.UTC(1601,0,1) + timeValue / 1000);
+WScript.Echo(w)
