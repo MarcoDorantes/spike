59ff1bf9e758cad529ce8a2e9c03dbec01af7ecc
MarcoDorantes <marcodorantes@hotmail.com>
2024-06-10 11:05:53 -0600

    Updated source/MSEdgeBookmarks

--- source/MSEdgeBookmarks/when.js
+++ source/MSEdgeBookmarks/when.js
@@ -1,3 +1,4 @@
+//https://stackoverflow.com/questions/70535929/how-to-determine-when-a-synced-bookmark-in-microsoft-edge-was-created
 //var timeValue = '13285408186887489';
 //var timeValue = '13228215968000000';
 var timeValue = '13362347413406690';
