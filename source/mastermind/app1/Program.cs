2c633ff3238c5d0dd97a5eda876e2325e9d977da
MarcoDorantes <marcodorantes@hotmail.com>
2024-09-22 22:29:42 -0500

    Added source/mastermind/pwsh/mastermind/app1

---
+++ source/mastermind/app1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+using static System.Console;
+
+namespace matermind
+{
+    class Exe
+    {
+        static void Main(string[] args)
+        {
+            var k = System.Console.ReadKey();
+            WriteLine($"\n{k.Key}\n");
+            WriteLine(string.Join(" ",Enum.GetNames(typeof(ConsoleKey))));
+        }
+    }
+}
