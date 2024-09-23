0d54a16294548a05a325234d81a241dfc75b2a43
MarcoDorantes <marcodorantes@hotmail.com>
2024-09-22 21:15:45 -0500

    Updated source/mastermind/pwsh/mastermind

--- source/mastermind/pwsh/mastermind.ps1
+++ source/mastermind/pwsh/mastermind.ps1
@@ -1,6 +1,7 @@
 param([switch]$startmode, [switch]$separator, [uint]$trylimit = 12)
 
 $MASTERMIND_VERSION = New-Object -TypeName System.Version -ArgumentList '1.0.0'
+$VALID_INPUT_KEYS_ARRAY = @([System.Enum]::GetNames([System.ConsoleKey]) | ? {$_ -match 'NumPad' -or $_ -match '^D\d' -or $_ -eq 'Spacebar'})
 
 function Get-GameVersion
 {
@@ -52,24 +53,68 @@ function Get-Hint($guess, $toguess)
     return [PSCustomObject]@{Whites=$white; Reds=$red}
 }
 
+function Read-GuessKeys
+{
+    $result = $null
+    $keys = New-Object -TypeName System.Collections.ArrayList
+    do
+    {
+        $k = [console]::ReadKey()
+        if($k.Key -eq [System.ConsoleKey]::Enter)
+        {
+            $text = New-Object -TypeName System.Text.StringBuilder
+            foreach($c in $keys) {$text.Append($c); Write-Host $c}
+            $result = "$($text)" 
+            break
+        }
+        if("$($k.Key)" -in $VALID_INPUT_KEYS_ARRAY -and $_.KeyChar -notin $keys) { $keys.Add($k.KeyChar) | Out-Null }
+    }while($true)
+    return $result
+}
+
+function Read-Guess($Prompt)
+{
+    $result = $null
+    Write-Host -NoNewline $Prompt
+    $result = Read-GuessKeys
+    <#
+    $text = New-Object -TypeName System.Text.StringBuilder
+    do
+    {
+        $k = [console]::ReadKey()
+        if($k.Key -eq [System.ConsoleKey]::Enter)
+        {
+            $result = "$($text)"
+            break
+        }
+        if("$($k.Key)" -in $VALID_INPUT_KEYS_ARRAY) { $text.Append($k.KeyChar) }
+    }while($true)
+    #>
+    return $result
+}
+
 function TopEntryPoint
 {
     "`n"
     $toguess = Get-GameCode
 #$toguess -join ' '
-    $askguess = 'Which are the four numbers?'
+    $askguess = 'Which are the four numbers? : '
     $askguesslength = New-Object -TypeName System.String -ArgumentList @(' ', $askguess.Length)
     [int]$trycount = 0
     do {
         if($trycount -ge $trylimit) { "`nYou reached the attempts limit of $($trylimit): The game won!!!`nThe numbers were $($toguess -join ' ')`n"; break;  }
-        $guess_input = Read-Host -Prompt $askguess
+#        $guess_input = Read-Host -Prompt $askguess
+        $guess_input = Read-Guess -Prompt $askguess
+#        Write-Host "[$($guess_input)]"
+#        return
         $guess = Get-Guess $guess_input $separator
         ++$trycount
         if(!$guess) { continue }
         if([System.Linq.Enumerable]::SequenceEqual([object[]]$guess,[object[]]$toguess)) { "`nYou won in $trycount attempts!!!`n"; break; }
-        $trylabel = "Attempt #$('{0,2:}' -f $trycount)>"
+        $trylabel = "Attempt #$('{0,-2}' -f $trycount)>"
         $hint = Get-Hint $guess $toguess
-        "$($askguesslength)  $('{0,-9}' -f $guess_input) $($trylabel) Misplaced: $($hint.Whites) Exact: $($hint.Reds)"
+#        "$($askguesslength)       $($trylabel) Misplaced: $($hint.Whites) Exact: $($hint.Reds)"
+        Write-Host "`r$($askguess)$($guess_input) $($trylabel) Misplaced: $($hint.Whites) Exact: $($hint.Reds)"
     }while($true)
 }
 
