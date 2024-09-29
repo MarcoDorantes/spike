9f85f57040417ccd8cf8520db9ed4088a7a3e522
MarcoDorantes <marcodorantes@hotmail.com>
2024-09-29 11:26:43 -0600

    Added .\source\back1

---
+++ source/back1/app1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace app1;
+
+using System;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+class Exe
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            lib1.Class1 x = new();
+            await x.Start(args);
+        }
+        catch(Exception ex)
+        {
+            for(int level=0; ex!=null; ex=ex.InnerException,++level) WriteLine($"[Level {level}] {ex.GetType().FullName}: {ex.Message}");
+        }
+    }
+}
---
+++ source/back1/lib1/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace lib1;
+
+using System.Threading.Tasks;
+
+using static System.Console;
+
+public class Class2
+{
+    public async Task Start(string[] args)
+    {
+        WriteLine($"{nameof(Class2)}.{nameof(Start)} begin");
+        await Task.Delay(100);
+        WriteLine($"{nameof(Class2)}.{nameof(Start)} end");
+    }
+}
+
+public class Class1
+{
+    public async Task Start(string[] args)
+    {
+        WriteLine($"{nameof(Class1)}.{nameof(Start)} begin");
+        Class2 y=new();
+        await y.Start(args);
+        WriteLine($"{nameof(Class1)}.{nameof(Start)} end");
+    }
+}
