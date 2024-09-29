5653f1a56843834c9d2fb5097893208bbd8fcef8
MarcoDorantes <marcodorantes@hotmail.com>
2024-09-29 14:19:46 -0600

    Updated .\source\back1

--- source/back1/work1/Program.cs
+++ source/back1/work1/Program.cs
@@ -1,7 +1,29 @@
 using work1;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-var host = builder.Build();
-host.Run();
+class Exe
+{
+    static void Main(string[] args)
+    {
+        System.Func<System.IServiceProvider, Worker> implementationFactory = CreateSimilarAsFound;
+
+        var builder = Host.CreateApplicationBuilder(args);
+        //builder.Services.AddHostedService<Worker>();
+        builder.Services.AddHostedService<Worker>(implementationFactory);
+
+        var host = builder.Build();
+        host.Run();
+    }
+    static Worker CreateSimilarAsFound(System.IServiceProvider factory) 
+    {
+        using ILoggerFactory logfactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger<Worker> logger = logfactory.CreateLogger<Worker>();
+        Worker result = new(logger);
+        return result;
+    }
+}/*
+logger.LogInformation("Hello World! Logging is {Description}.", "fun");
+*/
--- source/back1/work1/Worker.cs
+++ source/back1/work1/Worker.cs
@@ -1,5 +1,11 @@
 namespace work1;
 
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
@@ -11,13 +17,17 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("started at: {time}", DateTimeOffset.Now);
+        _logger.LogInformation(_logger.GetType().FullName);
         while (!stoppingToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(4000, stoppingToken);
+            _logger.LogInformation("next: {time}", DateTimeOffset.Now);
         }
+        _logger.LogInformation("ended at: {time}", DateTimeOffset.Now);
     }
 }
