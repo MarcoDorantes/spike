ffda7de388ed8574bf9c4da36ddfe9b2f41bebe7
MarcoDorantes <marcodorantes@hotmail.com>
2024-10-01 18:58:33 -0500

    Updated source/back1/work1/Program.cs: tried FileLoggerProvider<T>/FileLogger<T>

--- source/back1/work1/Program.cs
+++ source/back1/work1/Program.cs
@@ -1,14 +1,52 @@
 using work1;
 
+using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+
+class FileLogger<T> : ILogger<T>
+{
+    private string logfile;
+    private static readonly object sync = new();
+    public FileLogger(string file)
+    {
+        logfile = file;
+    }
+    #region ILogger<T>
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        if (formatter == null) return;
+        lock (sync)
+        {
+            System.Text.StringBuilder logline = new($"{logLevel} {DateTime.Now:s} {formatter(state, exception)}");
+            for (int level = 0; exception != null; exception = exception.InnerException, ++level) logline.AppendFormat("\n[Level {0}] {1}: {2} {3}", level, exception.GetType().FullName, exception.Message, exception.StackTrace);
+            System.IO.File.AppendAllText(logfile, $"{logline}\n");
+        }
+    }
+    #endregion
+}
+
+class FileLoggerProvider<T> : ILoggerProvider
+{
+    public string LogFile { get; internal set; }
+
+    #region ILoggerProvider
+    public ILogger CreateLogger(string categoryName) => new FileLogger<T>(LogFile);
+
+    public void Dispose(){}
+    #endregion
+}
 
 class Exe
 {
     static void Main(string[] args)
     {
-        System.Func<System.IServiceProvider, Worker> implementationFactory = CreateSimilarAsFound;
+        Func<IServiceProvider, Worker> implementationFactory = CreateSimilarAsFound;
 
         var builder = Host.CreateApplicationBuilder(args);
         //builder.Services.AddHostedService<Worker>();
@@ -19,8 +57,11 @@ class Exe
     }
     static Worker CreateSimilarAsFound(System.IServiceProvider factory)
     {
-        System.Console.WriteLine($"{nameof(factory)}: {factory?.GetType().FullName}");
-        using ILoggerFactory logfactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => builder.AddConsole());
+        var logfile = System.IO.Path.Combine(System.Environment.CurrentDirectory, $"worker_{DateTime.Now:yyyyMMdd-HHmmss}.log");
+        Console.WriteLine($"{nameof(factory)}: {factory?.GetType().FullName}");
+        Console.WriteLine($"{nameof(logfile)}: {logfile}");
+        //using ILoggerFactory logfactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => builder.AddConsole());
+        using ILoggerFactory logfactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => builder.AddProvider(new FileLoggerProvider<Worker>() { LogFile = logfile }));
         ILogger<Worker> logger = logfactory.CreateLogger<Worker>();
         Worker result = new(logger);
         return result;
