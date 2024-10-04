b17ad8ad3f11845748dceb1905c200aa3a1c2de6
MarcoDorantes <marcodorantes@hotmail.com>
2024-10-04 16:13:17 -0500

    Updated source/back1/back1.sln work1: added flog1.

---
+++ source/back1/flog1/FileLogger.cs
@@ -0,0 +1,92 @@
+﻿namespace flog1;
+
+using Microsoft.Extensions.Logging;
+
+public class FileLogger<T> : ILogger<T>, System.IDisposable
+{
+    private string logfile;
+    private static readonly object sync = new();
+
+    private System.Diagnostics.TextWriterTraceListener listener;
+    private System.IO.StreamWriter listenerWriter;
+
+    public FileLogger(string file)
+    {
+        logfile = file;
+#if TRACE
+        var logname = logfile;
+        System.Diagnostics.Trace.Listeners.Clear();
+        System.IO.FileInfo listenerFile = new(logname);
+        listenerWriter = listenerFile.CreateText();
+        System.Diagnostics.TextWriterTraceListener listener = new(listenerWriter);
+        System.Diagnostics.Trace.Listeners.Add(listener);
+        System.Diagnostics.Trace.AutoFlush = true;
+        System.Diagnostics.Trace.WriteLine($"Underlying logger type: {nameof(System.Diagnostics.TextWriterTraceListener)}");
+#else
+        WriteLogline(logfile, $"Underlying logger type: {nameof(System.IO.File)}.{nameof(System.IO.File.AppendAllText)}");
+#endif
+    }
+    #region ILogger<T>
+    public System.IDisposable BeginScope<TState>(TState state) where TState : notnull => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, System.Exception exception, System.Func<TState, System.Exception, string> formatter)
+    {
+        if (formatter == null) return;
+        System.Text.StringBuilder logline = new($"{logLevel} {System.DateTime.Now:s} {formatter(state, exception)}");
+        for (int level = 0; exception != null; exception = exception.InnerException, ++level) logline.AppendFormat("\n[Level {0}] {1}: {2} {3}", level, exception.GetType().FullName, exception.Message, exception.StackTrace);
+#if TRACE
+        System.Diagnostics.Trace.WriteLine($"{logline}");
+#else
+        lock (sync)
+        {
+            WriteLogline(logfile, $"{logline}");
+        }
+#endif
+    }
+    #endregion
+
+    private void WriteLogline(string logfile, string logline) => System.IO.File.AppendAllText(logfile, $"{logline}\n");
+
+    #region Disposable support
+    private bool disposedValue;
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            if (listener != null && System.Diagnostics.Trace.Listeners.Contains(listener))
+            {
+                System.Diagnostics.Trace.Listeners.Remove(listener);
+                listener?.Dispose();
+                listenerWriter?.Dispose();
+                listener = null;
+                listenerWriter = null;
+            }
+
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~FileLogger()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        System.GC.SuppressFinalize(this);
+    }
+    #endregion
+}
---
+++ source/back1/flog1/FileLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace flog1;
+
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+public class FileLoggerProvider<T> : ILoggerProvider
+{
+    private List<FileLogger<T>> loggers;
+
+    public FileLoggerProvider()
+    {
+        loggers = [];
+    }
+
+    public string LogFile { get; set; }
+
+    #region ILoggerProvider
+    public ILogger CreateLogger(string categoryName)
+    {
+        FileLogger<T> result = new(LogFile);
+        loggers.Add(result);
+        return result;
+    }
+
+    public void Dispose() { loggers.ForEach(x => x.Dispose()); System.Console.WriteLine($"Disposed {loggers.Count} loggers."); }
+    #endregion
+}
