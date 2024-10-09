f96f11c2adc707ddaa469e91173c23f388d827d8
MarcoDorantes <marcodorantes@hotmail.com>
2024-10-09 14:25:08 -0600

    Updated source\SymbolCmdlet

--- source/SymbolCmdlet/Symbol.Tests.ps1
+++ source/SymbolCmdlet/Symbol.Tests.ps1
@@ -2,29 +2,6 @@ BeforeAll {
     . $PSScriptRoot\Symbol.ps1
 }
 
-Describe Parse1 {
-    It SymbolID {
-        $asread = [PSCustomObject]@{Symbol='NAC/GCARSO A1'; Count=0}
-        $symbol = [SymbolObject]::new($asread)
-        $symbol.ID | Should -Be 'NAC/GCARSO A1'
-    }
-
-    It replace1 {
-        'NAC/GCARSO A1' -replace '~$','*' | Should -Be 'NAC/GCARSO A1'
-    }
-
-    It replace2 {
-        'NAC/GCARSO ~' -replace '~$','*' | Should -Be 'NAC/GCARSO *'
-    }
-
-    It parts1 {
-        $parts = Select-String -InputObject 'NAC/GCARSO A1' -Pattern '^(?<market>\w+)/(?<emisora>\w+) (?<serie>.+)$'
-        $parts.Matches[0].Groups['market'].Value | Should -Be 'NAC'
-        $parts.Matches[0].Groups['emisora'].Value | Should -Be 'GCARSO'
-        $parts.Matches[0].Groups['serie'].Value | Should -Be 'A1'
-    }
-}
-
 Describe Load {
     BeforeAll {
         $symbol = Read-SymbolTrace $PSScriptRoot\Symbols-20241008-171300.csv
