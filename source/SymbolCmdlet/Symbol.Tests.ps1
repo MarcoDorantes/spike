48d74a5a7cb2908df9440940ae41e616ce1959bb
MarcoDorantes <marcodorantes@hotmail.com>
2024-10-10 18:26:42 -0600

    Updated source\SymbolCmdlet

--- source/SymbolCmdlet/Symbol.Tests.ps1
+++ source/SymbolCmdlet/Symbol.Tests.ps1
@@ -2,7 +2,7 @@ BeforeAll {
     . $PSScriptRoot\Symbol.ps1
 }
 
-Describe Load1 {
+Describe LoadCount1 {
     BeforeAll {
         $symbol = Read-SymbolTrace $PSScriptRoot\Symbols-20241007-171300.csv
     }
@@ -37,7 +37,7 @@ Describe Load1 {
     }
 }
 
-Describe Load2 {
+Describe LoadCount2 {
     BeforeAll {
         $symbol = Read-SymbolTrace $PSScriptRoot\Symbols-20241008-171300.csv
     }
@@ -69,5 +69,59 @@ Describe Load2 {
         $symbolx.Market | Should -Be 'NAC'
         $symbolx.Counted | Should -Be 101476
         $symbolx.ConfigurableTopicSuffix | Should -Be 'NAC/ORBIA ~'
+    }
+}
+
+Describe derivation {
+    It counted1 {
+        $asread = [PSCustomObject]@{Symbol='NAC/GCARSO A1'; Count=2}
+        $symbol = [SymbolObject]::new($asread)
+        $symbol.ID | Should -Be 'NAC/GCARSO A1'
+        $symbol.StockSeries | Should -Be 'GCARSO A1'
+        $symbol.Emisora | Should -Be 'GCARSO'
+        $symbol.Serie | Should -Be 'A1'
+        $symbol.Market | Should -Be 'NAC'
+        $symbol.Counted | Should -Be 2
+        $symbol.ConfigurableTopicSuffix | Should -Be 'NAC/GCARSO A1'
+    }
+
+    It historic1 {
+        $asread = [PSCustomObject]@{
+            Date='6/30/2020 5:14:01 PM'
+            Symbol='MMM *'
+            Market='SIC'
+            QuoteCount=164
+        }
+        $symbol = [HistoricSymbol]::new($asread)
+        $symbol.ID | Should -Be 'SIC/MMM *'
+        $symbol.StockSeries | Should -Be 'MMM *'
+        $symbol.Emisora | Should -Be 'MMM'
+        $symbol.Serie | Should -Be '*'
+        $symbol.Market | Should -Be 'SIC'
+        $symbol.Counted | Should -Be 164
+        $symbol.ConfigurableTopicSuffix | Should -Be 'SIC/MMM ~'
+        '{0:s}' -f $symbol.When | Should -Be '2020-06-30T17:14:01'
+    }
+}
+
+Describe LoadHistory1 {
+    BeforeAll {
+        $history = Read-SymbolHistory $home\Downloads\MD_Tracked-Symbol-Quote-Utilization_2024-07-31_ByDate.csv
+    }
+
+    It load1 {
+        $history | Should -Not -BeNullOrEmpty
+    }
+
+    It historic1 {
+        $symbol = $history | select -Last 1
+        $symbol.ID | Should -Be 'SIC/TXN *'
+        $symbol.StockSeries | Should -Be 'TXN *'
+        $symbol.Emisora | Should -Be 'TXN'
+        $symbol.Serie | Should -Be '*'
+        $symbol.Market | Should -Be 'SIC'
+        $symbol.Counted | Should -Be 1
+        $symbol.ConfigurableTopicSuffix | Should -Be 'SIC/TXN ~'
+        '{0:s}' -f $symbol.When | Should -Be '2024-07-31T17:14:00'
     }
 }
--- source/SymbolCmdlet/Symbol.ps1
+++ source/SymbolCmdlet/Symbol.ps1
@@ -1,4 +1,4 @@
-class SymbolObject
+class SymbolBase
 {
     [PSCustomObject]$AsImported
     [string]$ID
@@ -9,10 +9,17 @@ class SymbolObject
     [System.UInt32]$Counted
     [string]$ConfigurableTopicSuffix
 
-    SymbolObject([PSCustomObject]$asread)
+    SymbolBase([PSCustomObject]$asread)
     {
         $this.AsImported = $asread
         $this.ID = $this.AsImported.Symbol -replace '~$','*'
+    }
+}
+
+class SymbolObject : SymbolBase
+{
+    SymbolObject([PSCustomObject]$asread) : base($asread)
+    {
         $parts = Select-String -InputObject $this.ID -Pattern '^(?<market>\w+)/(?<emisora>.+) (?<serie>.+)$'
         if(!$parts) { throw "Invalid symbol format ($($this.AsImported.Symbol))." }
         $this.StockSeries = @($parts.Matches[0].Groups['emisora'].Value, $parts.Matches[0].Groups['serie'].Value) -join ' '
@@ -21,6 +28,24 @@ class SymbolObject
         $this.Market = $parts.Matches[0].Groups['market'].Value
         $this.Counted = [System.UInt32]::Parse($this.AsImported.Count)
         $this.ConfigurableTopicSuffix = $this.AsImported.Symbol
+    }
+}
+
+class HistoricSymbol : SymbolBase
+{
+    [DateTime]$When
+
+    HistoricSymbol([PSCustomObject]$asread) : base($asread)
+    {
+        $this.ID = @($this.AsImported.Market, $this.ID) -join '/'
+        $this.StockSeries = $this.AsImported.Symbol
+        $parts = Select-String -InputObject $this.AsImported.Symbol -Pattern '^(?<emisora>.+) (?<serie>.+)$'
+        $this.Emisora = $parts.Matches[0].Groups['emisora'].Value
+        $this.Serie = $parts.Matches[0].Groups['serie'].Value
+        $this.Market = $this.AsImported.Market
+        $this.Counted = [System.UInt32]::Parse($this.AsImported.QuoteCount)
+        $this.ConfigurableTopicSuffix = $this.ID -replace '\*$','~'
+        $this.When = [DateTime]$this.AsImported.Date
     }
 }
 
@@ -39,11 +64,35 @@ function ConvertTo-SymbolObject
     }
 }
 
-function Read-SymbolTrace($file)
+function ConvertTo-HistoricSymbolObject
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [PSCustomObject]$AsImported
+    )
+    
+    process
+    {
+        $result = [HistoricSymbol]::new($AsImported)
+        $result
+    }
+}
+
+function Read-SymbolTrace([Parameter(Mandatory)][System.IO.FileInfo]$file)
 {
     Import-Csv $file | ConvertTo-SymbolObject
+}
+
+function Read-SymbolHistory([Parameter(Mandatory)][System.IO.FileInfo]$file)
+{
+    Import-Csv $file | ConvertTo-HistoricSymbolObject
 }
 <#
 $ss = Read-SymbolTrace .\source\SymbolCmdlet\Symbols-20241007-171300.csv
 $ss | sort -desc Counted -Stable| ? {$_.Market -eq 'NAC' -and $_.Emisora.StartsWith('O')}|select ID,Counted|measure -Sum Counted
+
+cat $home\Downloads\MD_Tracked-Symbol-Quote-Utilization_2024-07-31_ByDate.csv -first 2
+"Date","Symbol","Market","QuoteCount"
+"6/30/2020 5:14:01 PM","MMM *","SIC","164"
 #>
