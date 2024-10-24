9f9afc66cec844cbeb6098420e34d8c21cd7537f
MarcoDorantes <marcodorantes@hotmail.com>
2024-10-24 14:55:37 -0600

    Updated source\back1\spec1 tstates: checked async/await output to file system.

--- source/back1/spec1/flog1spec.cs
+++ source/back1/spec1/flog1spec.cs
@@ -8,13 +8,16 @@ using Microsoft.Extensions.Logging;
 [TestClass, TestCategory("Component")]
 public class app1spec
 {
-    private static string folder;
+    private static string folderbase;
 
     [ClassInitialize]
     public static void Init(TestContext context)
     {
-        folder = context.DeploymentDirectory;
+        folderbase = context.DeploymentDirectory;
     }
+
+    [ClassCleanup]
+    public static void UnInit() => Directory.EnumerateDirectories(folderbase).ToList().ForEach(f=>Directory.Delete(f,true));
 
     [TestMethod, Ignore]
     public void LogFileCreated()
@@ -26,7 +29,7 @@ public class app1spec
             FileInfo logfile = null;
             string filename_formatter(string category)
             {
-                logfile = new(Path.Combine(folder, $"{nameof(app1spec)}_{DateTime.Now:yyyyMMdd-HHmmss}.log"));
+                logfile = new(Path.Combine(folderbase, $"{nameof(app1spec)}_{DateTime.Now:yyyyMMdd-HHmmss}.log"));
                 return logfile.FullName;
             }
             filelogger_provider = new(filename_formatter);
@@ -46,41 +49,57 @@ public class app1spec
             filelogger_provider.Dispose();
         }
     }
-    async System.Threading.Tasks.Task SendAsync(int n)
+    async System.Threading.Tasks.Task SendAsync(int n, string folder, string tag)
     {
-        await System.IO.File.AppendAllTextAsync(@"F:\tep\log\tstate.log",$"{n}\n");
+        var filename=Path.Combine(folder, $"{tag}_{n}.log");
+        await File.AppendAllTextAsync(filename,$"{n}");
     }
-    void sendm(int n)
+    void sendm(int n, string folder, string tag)
     {
-      //var t=SendAsync(n);t.Wait();
-      //System.Threading.Tasks.Task.Run(async () => await SendAsync(n).ConfigureAwait(false));
-      //System.IO.File.AppendAllText(@"C:\temp\log\tstate0.log",$"{n}\n");
+      //var t=SendAsync(n, folder, tag);t.Wait();
+      System.Threading.Tasks.Task.Run(async () => await SendAsync(n, folder, tag).ConfigureAwait(false));
     }
-    [TestMethod, Ignore]
+    [TestMethod]
     public void tstate()
     {
-        for(int k=0;k<1000;++k) sendm(k);
+        var folder=Path.Combine(folderbase, nameof(tstate));
+        if(Directory.Exists(folder)) Directory.Delete(folder, true);
+        Directory.CreateDirectory(folder);
+        for(int k=0;k<1000;++k) sendm(k, folder, nameof(tstate));
         System.Threading.Thread.Sleep(15000);
+        var index = Directory.EnumerateFiles(folder,"*.log").Select(f=>int.Parse(File.ReadAllText(f))).Distinct();
+        Assert.AreEqual(1000,index.Count());
+        Directory.EnumerateFiles(folder,"*.log").ToList().ForEach(f=>File.Delete(f));
     }
-    class Exe
+    class Exe(string folder, string tag):IDisposable
     {
         public System.Threading.Tasks.Task task;
+        public string filename;
         public async System.Threading.Tasks.Task SendAsync(int n)
         {
-            await System.IO.File.AppendAllTextAsync(@$"F:\tep\log\tstate_{n}.log",$"{n}\n");
+            filename = Path.Combine(folder, $"{tag}_{n}.log");
+            await File.AppendAllTextAsync(filename,$"{n}");
         }
         public void sendm(int n)
         {
             task = System.Threading.Tasks.Task.Run(async () => await SendAsync(n).ConfigureAwait(false));
         }
+        public void Dispose(){if(File.Exists(filename)) File.Delete(filename);}
     }
     [TestMethod]
     public void tstates()
     {
-        var exes=Enumerable.Range(0,1000).Aggregate(new System.Collections.Generic.List<Exe>(),(whole,next)=>{whole.Add(new Exe());return whole;});
+        var folder=Path.Combine(folderbase, nameof(tstates));
+        if(Directory.Exists(folder)) Directory.Delete(folder, true);
+        Directory.CreateDirectory(folder);
+        var exes=Enumerable.Range(0,1000).Aggregate(new System.Collections.Generic.List<Exe>(),(whole,next)=>{whole.Add(new Exe(folder, nameof(tstates)));return whole;});
         for(int k=0;k<1000;++k) exes[k].sendm(k);
         System.Threading.Thread.Sleep(15000);
         Assert.AreEqual(1000,exes.Where(e=>e.task!=null).Count());
         System.Threading.Tasks.Task.WaitAll(exes.Where(e=>e.task!=null).Select(e=>e.task).ToArray());
+        //ls F:\tep\log\tstateB*.log|cat|%{[PSCustomObject]@{Index=[int]::Parse($_)}}|select -Unique Index|measure
+        var index = Directory.EnumerateFiles(folder,"*.log").Select(f=>int.Parse(File.ReadAllText(f))).Distinct();
+        Assert.AreEqual(1000,index.Count());
+        exes.ForEach(e=>e.Dispose());
     }
 }
