c78aa6624e91592d3987c4300014a55aca1153bb
MarcoDorantes <marcodorantes@hotmail.com>
2025-02-08 13:55:00 -0600

    Updated source\Kaprekar

--- source/Kaprekar/Kaprekar.Tests.ps1
+++ source/Kaprekar/Kaprekar.Tests.ps1
@@ -40,4 +40,43 @@ Describe sums {
     It sum1 {
         GetSum 6174 | Should -Be 6174
     }
+    It sum1 {
+        GetSum 1234 | Should -Be 3087
+    }
+    It sum1 {
+        GetSum 3087 | Should -Be 8352
+    }
+    It sum1 {
+        GetSum 8352 | Should -Be 6174
+    }
+    It sumcount {
+        GetSumCount 1234 | Should -Be 3
+    }
+    It sumcount {
+        GetSumCount 3087 | Should -Be 2
+    }
+    It sumcount {
+        GetSumCount 8352 | Should -Be 1
+    }
+    It sumcount {
+        GetSumCount 1000 7000 | Should -Be -1
+    }
+    It sumcount {
+        GetSumCount 9998 7000 | Should -Be -1
+    }
+    It sumcount {
+        1000 | GetSumCount | Should -Be -1
+    }
+    It sumcount {
+        1000..9999 | ?{ (GetSumCount $_) -eq -1} | measure | select -expand Count | Should -Be 2057
+    }
+    It sumcount {
+        1000..9999 | ?{ (GetSumCount $_) -ne -1} | measure | select -expand Count | Should -Be (9000-2057)
+    }
+    It sumcount {
+        1000..9999 | ?{ (GetSumCount $_) -ne -1} | select -last 3| Join-String -Separator '|' | Should -Be '9995|9996|9997'
+    }
+    It sumcount {
+        1..1000 | ?{ (GetSumCount $_) -ne -1} | select -last 3| Join-String -Separator '|' | Should -Be ''
+    }
 }
--- source/Kaprekar/Kaprekar.ps1
+++ source/Kaprekar/Kaprekar.ps1
@@ -11,3 +11,20 @@ function GetSumB([int]$n)
 }
 
 function GetSum([int]$n) { return (GetSumA $n) - (GetSumB $n) }
+
+function GetSumCount
+{
+    [CmdletBinding()]
+    param([Parameter(Mandatory,ValueFromPipeline)][int]$n, [int]$limit = 7)
+
+    [int]$r = $n
+    [int]$c = 0
+    do
+    {
+        $r = GetSum $r
+        ++$c
+        if($c -ge $limit) { return [int](-1) }
+    }
+    while($r -ne 6174)
+    return $c
+}
