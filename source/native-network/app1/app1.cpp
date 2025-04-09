3d0a3f08c630813a1af067056254f1315506cb37
MarcoDorantes <marcodorantes@hotmail.com>
2025-04-09 12:32:45 -0600

    https://qr.ae/pArsxo

--- source/native-network/app1/app1.cpp
+++ source/native-network/app1/app1.cpp
@@ -3,14 +3,33 @@
 
 #include <iostream>
 
+class YY
+{
+    int ID;
+public:
+    YY() :ID(-1) { std::cout << "YY:ctor()\n"; }
+    YY(int n) {}
+    YY(const YY& other):ID(other.ID) { std::cout << "YY:ctor(YY)\n"; }
+//  YY(const YY& other) = delete; //Copy not allowed.
+};
+
 class XX
 {
     int ID;
+    YY y;
 
-    int getid(int n) { if (n < 0) throw std::exception("Invalid ID"); return n; }
+    int getid(int n)
+    {
+        std::cout << "XX:ctor("<<n<<")\n";
+        if (n < 0) throw std::exception("Invalid ID");
+        return n;
+    }
 
 public:
+    XX():ID(-1) { std::cout << "XX:ctor()\n"; }
     XX(int n):ID(getid(n)) {}
+//  XX(const XX& other):ID(other.ID) { std::cout << "XX:ctor(XX)\n"; }
+//  XX(const XX& other) = delete; //Copy not allowed.
 
     void show(std::ostream& o)
     {
@@ -18,11 +37,45 @@ public:
     }
 };
 
+struct point {
+    int x;
+    int y;
+};
+
+void fpoint(struct point p)
+{
+    printf("%d %d\n", p.x, p.y);
+    p.x = 2;
+    p.y = 3;
+}
+
+void passStructByVal()
+{
+    struct point pxx = { 100,200 };
+    fpoint(pxx);
+    printf("%d %d\n", pxx.x, pxx.y);
+}
+
+void fXX(XX aa)
+{
+    aa.show(std::cout);
+}
+
+void passClassByVal()
+{
+    XX a{ 10 };
+    fXX(a);
+    a.show(std::cout);
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
     try
     {
+        passStructByVal();
+        passClassByVal();
+
         XX x{ -1 };
         x.show(std::cout);
     }
