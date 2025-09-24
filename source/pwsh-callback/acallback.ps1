c92bce52643531e912fac6ed03152651832c1194
MarcoDorantes <marcodorantes@hotmail.com>
2025-09-24 08:49:02 -0600

    Added source/pwsh-callback/acallback.ps1

---
+++ source/pwsh-callback/acallback.ps1
@@ -0,0 +1,24 @@
+Set-StrictMode -Version 3.0
+
+function FF([scriptblock]$f, [scriptblock]$g)
+{
+    'here'
+    if($f)
+    {
+       #$f.GetType().FullName # System.Management.Automation.ScriptBlock
+       $r = $f.Invoke($null)
+       if($r){"`t`$f = $f"}else{"`t`$f = null"}
+    }
+    if($g)
+    {
+
+        $ops = @(1,'name1')
+        $x = $g.Invoke($ops)
+        if($x)
+        {
+            "`t`$x ($($x.Count)):`n`t`t$($x -join "`n`t`t")"
+        }
+    }
+}
+
+FF {Write-Host 'here2'} {param([int]$n, [string]$name) Write-Host "$n = $name"; return @(1..3)}
