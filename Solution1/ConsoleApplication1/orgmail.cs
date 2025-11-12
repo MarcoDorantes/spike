b24366f1d8f0d0da9caba6a94ac64ac506c7ec9b
MarcoDorantes <marcodorantes@hotmail.com>
2025-11-12 11:45:27 -0600

    Updated Solution1/ConsoleApplication1/orgmail.cs: added links for Oct-2026

--- Solution1/ConsoleApplication1/orgmail.cs
+++ Solution1/ConsoleApplication1/orgmail.cs
@@ -54,6 +54,8 @@ https://learn.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online
 https://learn.microsoft.com/en-us/graph/migrate-exchange-web-services-api-mapping
 https://learn.microsoft.com/en-us/graph/migrate-exchange-web-services-overview?source=recommendations
 https://learn.microsoft.com/en-us/graph/migrate-exchange-web-services-authentication
+https://learn.microsoft.com/en-us/entra/identity-platform/quickstart-configure-app-access-web-apis
+https://learn.microsoft.com/en-us/graph/permissions-reference
 
 https://aka.ms/AAgzk1b | https://docs.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online/deprecation-of-basic-authentication-exchange-online
 https://developer.microsoft.com/en-us/graph/blogs/upcoming-changes-to-exchange-web-services-ews-api-for-office-365/
