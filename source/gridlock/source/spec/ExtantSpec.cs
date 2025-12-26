91bc9f778b49722c1faba459a8cc344eb5397263
MarcoDorantes <marcodorantes@hotmail.com>
2025-12-25 18:22:31 -0600

    Updated source: added gridlock.

---
+++ source/gridlock/source/spec/ExtantSpec.cs
@@ -0,0 +1,29 @@
+﻿namespace spec;
+
+using static System.Console;
+
+/*
+https://www.spinmaster.com/en-US/brands/rubiks/rubik-s-cube-gridlock
+
+dotnet test .\source\gridlock\source\spec\ -c Release --logger "console;verbosity=detailed"
+*/
+
+[TestClass]
+public sealed class ExtantSpec
+{
+    void display(char[,] g)
+    {
+        char[][] _ = new char[1][];
+        WriteLine(_.GetType().FullName);//System.Char[][]
+
+        WriteLine(g.GetType().FullName);//System.Char[,]
+        WriteLine(g[0,0]);
+    }
+    [TestMethod]
+    public void grid1()
+    {
+        char[,] grid = new char[8,8];
+        display(grid);
+        Assert.AreEqual(0,grid[0,0]);
+    }
+}
